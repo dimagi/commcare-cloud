df60bd31ee6afcd23ba74db20a5881cd87b79d38
Tyler Sheffels <tsheffels@dimagi.com>
2014-10-14 18:24:55 -0400

    Adds support for installing jdk - Needs to be added to the appropriate playbook

---
+++ ansible/roles/jdk/files/install_java.sh
@@ -0,0 +1,14 @@
+set -e
+wget http://files.dimagi.com/jdk7-x64.tar.gz -O /tmp/jdk7-x64.tar.gz
+tar -xvf /tmp/jdk7-x64.tar.gz -C /tmp/
+mkdir -p /usr/lib/jvm
+cd /tmp
+rm /usr/lib/jvm/jdk1.7.0 || true
+mv jdk1.7.0_45/ /usr/lib/jvm/
+ln -s /usr/lib/jvm/jdk1.7.0_45/ /usr/lib/jvm/jdk1.7.0
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 1
+update-alternatives --config java
+java -version
+echo 'java=installed' > /usr/lib/jvm/success.txt
