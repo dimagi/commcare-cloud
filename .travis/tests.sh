354d611523bf2af7cb87cef670a6fd8a1afa0c31
Ben <rudolphben@gmail.com>
2016-01-24 23:48:57 -0500

    Add sudo

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -5,9 +5,8 @@ test_syntax() {
 }
 
 test_localsettings() {
-  mkdir -p /home/cchq/www/dev/current/.git
-  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev.yml' --tags=commcarehq
-  python -m py_compile /home/cchq/www/dev/current/localsettings.py
+  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev.yml' --tags=commcarehq --skip-tags=slow
+  sudo python -m py_compile /home/cchq/www/dev/current/localsettings.py
 }
 
 test_syntax
