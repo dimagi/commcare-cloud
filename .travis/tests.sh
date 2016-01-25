e70de0245efc84dfda862662b240db24214b833b
Ben <rudolphben@gmail.com>
2016-01-24 23:39:23 -0500

    use commcarehq role

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -5,8 +5,8 @@ test_syntax() {
 }
 
 test_localsettings() {
-  mkdir -p /home/cchq/www/dev/current
-  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev.yml' --tags=localsettings
+  mkdir -p /home/cchq/www/dev/current/.git
+  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev.yml' --tags=commcarehq
   python -m py_compile /home/cchq/www/dev/current/localsettings.py
 }
 
