8b373a4be5ff910b74412d2baacaa51390b2acd9
Ben <rudolphben@gmail.com>
2016-01-25 00:08:37 -0500

    add a few cmds back

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -5,7 +5,7 @@ test_syntax() {
 }
 
 test_localsettings() {
-  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev.yml' --tags=commcarehq --skip-tags=slow
+  ansible-playbook -i ansible/inventories/test ansible/deploy_stack.yml -e '@ansible/vars/dev.yml' --tags=commcarehq
   sudo python -m py_compile /home/cchq/www/dev/current/localsettings.py
 }
 
