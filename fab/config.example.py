581e54f3ad1c947ece29630c5d4145c6c52fdb08
Ben <rudolphben@gmail.com>
2016-04-22 01:35:55 -0400

    Add comment to create github api key

--- fab/config.example.py
+++ fab/config.example.py
@@ -1,1 +1,3 @@
+# To generate a Github apikey follow these instructions: https://github.com/blog/1509-personal-api-tokens
+# Minimally you will need a key with repo -> public_repo
 GITHUB_APIKEY = ''
