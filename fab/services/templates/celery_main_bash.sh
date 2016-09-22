21d1fc933b7a9b5e6de92a2a0d0db4d3db547bd7
Ben Rudolph <rudolphben@gmail.com>
2016-09-22 11:51:31 -0400

    Add timestamp stuffix

--- fab/services/templates/celery_main_bash.sh
+++ fab/services/templates/celery_main_bash.sh
@@ -16,6 +16,6 @@
 # free to start another bash process under supervisor.
 trap 'kill -TERM $PID' TERM INT
 TIMESTAMP=`date +%s`
-{{ new_relic_command }}{{ virtualenv_current }}/bin/python {{ code_current }}/manage.py celery worker --queues=celery --events --loglevel=INFO --hostname={{ host_string }}_main.${TIMESTAMP} --maxtasksperchild=5 --autoscale={{ celery_params.concurrency }},0 -Ofair &
+{{ new_relic_command }}{{ virtualenv_current }}/bin/python {{ code_current }}/manage.py celery worker --queues=celery --events --loglevel=INFO --hostname={{ host_string }}_main.${TIMESTAMP}_timestamp --maxtasksperchild=5 --autoscale={{ celery_params.concurrency }},0 -Ofair &
 PID=$!
 wait $PID
