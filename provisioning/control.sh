e35787d8d3c25d5e50c176e47a995d4dad22d32c
Sravan Reddy <sravfeyn@gmail.com>
2016-10-31 19:22:43 +0530

    Revert "Merge master; resolve conflicts"
    This reverts commit 4cf8249e9ddc21e197714935c38f1b55a4e6d16b, reversing
    changes made to ddce6473b9f7161b181eeb00b21c84372b4a31b6.

--- provisioning/control.sh
+++ provisioning/control.sh
@@ -1,5 +1,9 @@
 sudo apt-get update
-sudo apt-get install -y python-dev python-pip libffi-dev git
+sudo apt-get install -y python-software-properties
+sudo apt-get install -y python-netaddr
+sudo apt-add-repository ppa:ansible/ansible-1.9
+sudo apt-get update
+sudo apt-get install -y ansible
 
 sudo cp /vagrant/provisioning/id_rsa /home/vagrant/.ssh/id_rsa
 sudo chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
@@ -8,12 +12,6 @@ sudo chmod 400 /home/vagrant/.ssh/id_rsa
 ssh-keyscan 192.168.33.15 >> /home/vagrant/.ssh/known_hosts
 ssh-keyscan 192.168.33.16 >> /home/vagrant/.ssh/known_hosts
 ssh-keyscan 192.168.33.17 >> /home/vagrant/.ssh/known_hosts
-
-ln -s /vagrant /home/vagrant/commcarehq-ansible
-mkdir /home/vagrant/commcare-hq-deploy
-sudo pip install virtualenv virtualenvwrapper
-sudo -H -u vagrant /vagrant/control/init.sh
-echo '[ -t 1 ] && source ~/init-ansible' >> /home/vagrant/.profile
 
 echo "Provision completed! Now ssh into the control box by:
 
