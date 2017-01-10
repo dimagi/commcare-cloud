25674f4bea7448d9f8b01ec411aaa04d73711c68
Ben <rudolphben@gmail.com>
2017-01-10 21:18:32 +0200

    extract updating code from previous release

--- fab/operations/release.py
+++ fab/operations/release.py
@@ -29,30 +29,7 @@ def update_code(git_tag, use_current_release=False):
     # If not updating current release,  we are making a new release and thus have to do cloning
     # we should only ever not make a new release when doing a hotfix deploy
     if not use_current_release:
-        if files.exists(env.code_current):
-            with cd(env.code_current):
-                submodules = sudo("git submodule | awk '{ print $2 }'").split()
-        with cd(env.code_root):
-            if files.exists(env.code_current):
-                local_submodule_clone = []
-                for submodule in submodules:
-                    local_submodule_clone.append('-c')
-                    local_submodule_clone.append(
-                        'submodule.{submodule}.url={code_current}/.git/modules/{submodule}'.format(
-                            submodule=submodule,
-                            code_current=env.code_current
-                        )
-                    )
-
-                sudo('git clone --recursive {} {}/.git {}'.format(
-                    ' '.join(local_submodule_clone),
-                    env.code_current,
-                    env.code_root
-                ))
-                sudo('git remote set-url origin {}'.format(env.code_repo))
-            else:
-                sudo('git clone {} {}'.format(env.code_repo, env.code_root))
-
+        _update_code_from_previous_release()
     with cd(env.code_root if not use_current_release else env.code_current):
         sudo('git remote prune origin')
         sudo('git fetch origin --tags -q')
@@ -64,6 +41,32 @@ def update_code(git_tag, use_current_release=False):
         sudo("git clean -ffd")
         # remove all .pyc files in the project
         sudo("find . -name '*.pyc' -delete")
+
+
+def _update_code_from_previous_release():
+    if files.exists(env.code_current):
+        with cd(env.code_current):
+            submodules = sudo("git submodule | awk '{ print $2 }'").split()
+    with cd(env.code_root):
+        if files.exists(env.code_current):
+            local_submodule_clone = []
+            for submodule in submodules:
+                local_submodule_clone.append('-c')
+                local_submodule_clone.append(
+                    'submodule.{submodule}.url={code_current}/.git/modules/{submodule}'.format(
+                        submodule=submodule,
+                        code_current=env.code_current
+                    )
+                )
+
+            sudo('git clone --recursive {} {}/.git {}'.format(
+                ' '.join(local_submodule_clone),
+                env.code_current,
+                env.code_root
+            ))
+            sudo('git remote set-url origin {}'.format(env.code_repo))
+        else:
+            sudo('git clone {} {}'.format(env.code_repo, env.code_root))
 
 
 @roles(ROLES_ALL_SRC)
