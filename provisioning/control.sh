b6b586a932d4240d5830aeeb015e48902507b298
Norman Hooper <nhooper@dimagi.com>
2017-01-20 15:07:07 +0200

    Add comment, and tweak for init.sh on vagrant

--- provisioning/control.sh
+++ provisioning/control.sh
@@ -13,6 +13,9 @@ ssh-keyscan 192.168.33.17 >> /home/vagrant/.ssh/known_hosts
 sudo chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
 
 ln -s /vagrant /home/vagrant/commcarehq-ansible
+# Prevent control/init.sh from trying to clone (non-existent) /home/ansible/commcarehq-ansible-secrets.git
+mkdir /home/vagrant/commcarehq-ansible/config
+# Prevent control/init.sh from trying to clone commcare-hq-deploy
 mkdir /home/vagrant/commcare-hq-deploy
 sudo pip install virtualenv virtualenvwrapper
 sudo -H -u vagrant /vagrant/control/init.sh
