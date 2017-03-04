08332a465aee4936e27358fa4c6ef507e1494d6c
Simon Kelly <skelly@dimagi.com>
2017-03-04 12:31:55 +0200

    add pgbouncer zabbix conf

--- ansible/roles/zabbix_postgres/README.md
+++ ansible/roles/zabbix_postgres/README.md
@@ -2,5 +2,10 @@
 
 In Zabbix UI:
 
+  * Install the PostgreSQL template
+    * http://cavaliercoder.com/libzbxpgsql/documentation/template-installation/
+    * https://github.com/cavaliercoder/libzbxpgsql/blob/master/templates/Template_PostgreSQL_Server_3.0.xml
   * Update template macro for "Template App PostgreSQL" template:
     * {$PG_CONN} => host=localhost port=5432 user=devreadonly connect_timeout=10
+  * Add the pgbouncer template
+    * https://github.com/lesovsky/zabbix-extensions/blob/master/files/pgbouncer/pgbouncer-extended-template.xml
---
+++ ansible/roles/zabbix_postgres/files/pgbouncer.pool.discovery.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Author: 	Lesovsky A.V.
+# Description:	Pgbouncer pools auto-discovery
+# https://github.com/lesovsky/zabbix-extensions/blob/master/files/pgbouncer
+
+if [ ! -f ~zabbix/.pgpass ]; then echo "ERROR: ~zabbix/.pgpass not found" ; exit 1; fi
+
+hostname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f1)
+port=$(head -n 1 ~zabbix/.pgpass |cut -d: -f2)
+username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
+dbname="pgbouncer"
+
+if [ '*' = "$hostname" ]; then hostname="127.0.0.1"; fi
+
+poollist=$(psql -h $hostname -p $port -U $username -ltAF: --dbname=$dbname -c "show pools" |cut -d: -f1 |grep -v ^pgbouncer)
+
+echo -n '{"data":['
+for pool in $poollist; do echo -n "{\"{#POOLNAME}\": \"$pool\"},"; done |sed -e 's:\},$:\}:'
+echo -n ']}'
