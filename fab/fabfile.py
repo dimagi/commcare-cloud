81e547ab5f7c9f058c7fe8a7007681e7594b8cbb
Simon Kelly <skelly@dimagi.com>
2017-03-21 14:34:31 +0200

    add env var for offline releases folder

--- fab/fabfile.py
+++ fab/fabfile.py
@@ -141,7 +141,8 @@ def _setup_path():
     env.services = posixpath.join(env.code_root, 'services')
     env.jython_home = '/usr/local/lib/jython'
     env.db = '%s_%s' % (env.project, env.environment)
-    env.offline_code_dir = posixpath.join('/home/{}/releases/{}'.format(env.user, env.deploy_metadata.timestamp))
+    env.offline_releases = posixpath.join('/home/{}/releases'.format(env.user))
+    env.offline_code_dir = posixpath.join('{}/{}'.format(env.offline_releases, 'offline'))
 
 
 def load_env(env_name):
