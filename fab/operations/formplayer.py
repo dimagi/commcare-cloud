d072846d124412cad0f0af86fd0f1e70df76f686
Simon Kelly <skelly@dimagi.com>
2017-08-22 15:38:05 +0200

    support rolling back formplayer

--- fab/operations/formplayer.py
+++ fab/operations/formplayer.py
@@ -1,10 +1,13 @@
+import datetime
 import os
 
+from fabric import utils
 from fabric.api import roles, env, sudo
-from fabric.operations import put
+from fabric.context_managers import cd
+from fabric.contrib import console
 from fabric.contrib import files
 
-from ..const import ROLES_FORMPLAYER, FORMPLAYER_BUILD_DIR, OFFLINE_STAGING_DIR
+from ..const import ROLES_FORMPLAYER, FORMPLAYER_BUILD_DIR, DATE_FMT
 
 
 @roles(ROLES_FORMPLAYER)
@@ -19,7 +22,12 @@ def build_formplayer(use_current_release=False):
     else:
         jenkins_formplayer_build_url = 'https://jenkins.dimagi.com/job/formplayer/lastSuccessfulBuild/artifact/build/libs/formplayer.jar'
 
-    sudo('wget -nv {} -O {}/formplayer.jar'.format(jenkins_formplayer_build_url, build_dir))
+    release_name = 'formplayer__{}'.format(datetime.datetime.utcnow().strftime(DATE_FMT))
+    sudo('wget -nv {} -O {}/{}'.format(jenkins_formplayer_build_url, build_dir, release_name))
+    sudo('ln -sf {build_dir}/{release_name} {build_dir}/formplayer.jar'.format(
+        build_dir=build_dir,
+        release_name=release_name
+    ))
 
 
 @roles(ROLES_FORMPLAYER)
@@ -32,3 +40,39 @@ def offline_build_formplayer():
         env.offline_code_dir,
         build_dir
     ))
+
+
+@roles(ROLES_FORMPLAYER)
+def rollback_formplayer():
+    build_dir = os.path.join(env.code_current, FORMPLAYER_BUILD_DIR)
+    with cd(build_dir):
+        previous_build_paths = sudo('find . -name "{}*"'.format('formplayer__')).strip()
+        if not previous_build_paths:
+            utils.abort('No formplayer builds to rollback to.')
+
+        builds = sorted(_get_builds(previous_build_paths.split('\n')), reverse=True)
+        if not builds:
+            utils.abort('No formplayer builds to rollback to.')
+
+        rollback_build = builds[0]
+        if not console.confirm('Confirm rollback to "{}"'.format(rollback_build), default=False):
+            utils.abort('Action aborted.')
+
+        sudo('ln -sf {build_dir}/{rollback} {build_dir}/formplayer.jar'.format(
+            build_dir=build_dir,
+            rollback=rollback_build
+        ))
+
+
+def _get_builds(build_paths):
+    builds = []
+    for path in build_paths:
+        filename = os.path.basename(path)
+        try:
+            _, date_to_delete_string = filename.split('formplayer__')
+            datetime.datetime.strptime(date_to_delete_string, DATE_FMT)
+        except ValueError:
+            continue
+        else:
+            builds.append(filename)
+    return builds
