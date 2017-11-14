e704f73c805d349aa06985dec2dec6ffdde807b2
Daniel Roberts <droberts@dimagi.com>
2017-11-14 16:26:47 -0500

    use correct check for ~/.commcare-cloud dirs

--- control/init.sh
+++ control/init.sh
@@ -39,10 +39,10 @@ pip install -e ~/commcarehq-ansible/commcare-cloud
 
 # convenience: . init-ansible
 [ ! -f ~/init-ansible ] && ln -s ~/commcarehq-ansible/control/init.sh ~/init-ansible
-[ ! -f ~/.commcare-cloud ] && mkdir ~/.commcare-cloud
-[ ! -f ~/.commcare-cloud/ansible ] && ln -s ~/commcarehq-ansible/ansible ~/.commcare-cloud/
-[ ! -f ~/.commcare-cloud/vars ] && ln -s ~/commcarehq-ansible/ansible/vars ~/.commcare-cloud/
-[ ! -f ~/.commcare-cloud/inventory ] && ln -s ~/commcare-hq-deploy/fab/inventory ~/.commcare-cloud/
+[ ! -d ~/.commcare-cloud ] && mkdir ~/.commcare-cloud
+[ ! -d ~/.commcare-cloud/ansible ] && ln -s ~/commcarehq-ansible/ansible ~/.commcare-cloud/
+[ ! -d ~/.commcare-cloud/vars ] && ln -s ~/commcarehq-ansible/ansible/vars ~/.commcare-cloud/
+[ ! -d ~/.commcare-cloud/inventory ] && ln -s ~/commcare-hq-deploy/fab/inventory ~/.commcare-cloud/
 
 alias ap='ansible-playbook -u ansible -i ../../commcare-hq-deploy/fab/inventory/$ENV -e "@vars/$ENV/${ENV}_vault.yml" -e "@vars/$ENV/${ENV}_public.yml" --ask-vault-pass'
 alias aps='ap deploy_stack.yml'
