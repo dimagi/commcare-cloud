f8a6152d254280415f0f187b0487f81eec7130f9
Daniel Roberts <droberts@dimagi.com>
2017-11-17 18:59:28 -0500

    add command to bootstrap users on a new machine
    as well as support for some vars in *_public.yml that were
    useful for deploying to AWS. Examples use:
    
    commcare_cloud_root_user: ubuntu
    commcare_cloud_pem: /path/to/identity.pem
    commcare_cloud_strict_host_key_checking: no
    commcare_cloud_use_value: no

--- commcare-cloud/commcare_cloud.py
+++ commcare-cloud/commcare_cloud.py
@@ -6,6 +6,7 @@ from six.moves import input, shlex_quote
 from argparse import ArgumentParser
 import subprocess
 from clint.textui import puts, colored
+import yaml
 
 
 def ask(message):
@@ -22,6 +23,12 @@ def arg_branch(parser):
     parser.add_argument('--branch', default='master', help=(
         "the name of the commcarehq-ansible git branch to run against, if not master"
     ))
+
+
+def get_public_vars(environment):
+    filename = os.path.expanduser('~/.commcare-cloud/vars/{env}/{env}_public.yml'.format(env=environment))
+    with open(filename) as f:
+        return yaml.load(f)
 
 
 class AnsiblePlaybook(object):
@@ -44,30 +51,50 @@ class AnsiblePlaybook(object):
     @staticmethod
     def run(args, unknown_args):
         check_branch(args)
+        public_vars = get_public_vars(args.environment)
+        pem = public_vars.get('commcare_cloud_pem', None)
+        strict_host_key_checking = public_vars.get('commcare_cloud_strict_host_key_checking', True)
+        ask_vault_pass = public_vars.get('commcare_cloud_use_value', True)
 
-        def ansible_playbook(environment, playbook, vault_password, *cmd_args):
+        def ansible_playbook(environment, playbook, *cmd_args):
             cmd_parts = (
                 'ANSIBLE_CONFIG={}'.format(os.path.expanduser('~/.commcare-cloud/ansible/ansible.cfg')),
                 'ansible-playbook',
                 os.path.expanduser('~/.commcare-cloud/ansible/{playbook}'.format(playbook=playbook)),
-                '-u', 'ansible',
                 '-i', os.path.expanduser('~/.commcare-cloud/inventory/{env}'.format(env=environment)),
                 '-e', '@{}'.format(os.path.expanduser('~/.commcare-cloud/vars/{env}/{env}_vault.yml'.format(env=environment))),
                 '-e', '@{}'.format(os.path.expanduser('~/.commcare-cloud/vars/{env}/{env}_public.yml'.format(env=environment))),
-                '--vault-password-file=/bin/cat',
                 '--diff',
             ) + cmd_args
+
+            if '-u' not in unknown_args:
+                cmd_args += ('-u', 'ansible')
+
+            if ask_vault_pass:
+                cmd_parts += ('--vault-password-file=/bin/cat',)
+
+            common_ssh_args = []
+            if pem:
+                common_ssh_args.extend(['-i', pem])
+            if not strict_host_key_checking:
+                common_ssh_args.append('-o StrictHostKeyChecking=no')
+
+            if common_ssh_args:
+                cmd_parts += ('--ssh-common-args', ' '.join(shlex_quote(arg) for arg in common_ssh_args))
             cmd = ' '.join(shlex_quote(arg) for arg in cmd_parts)
             print(cmd)
             p = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
-            p.communicate(input='{}\n'.format(vault_password))
+            if ask_vault_pass:
+                p.communicate(input='{}\n'.format(get_ansible_vault_password()))
+            else:
+                p.communicate()
             return p.returncode
 
         def run_check():
-            return ansible_playbook(args.environment, args.playbook, get_ansible_vault_password(), '--check', *unknown_args)
+            return ansible_playbook(args.environment, args.playbook, '--check', *unknown_args)
 
         def run_apply():
-            return ansible_playbook(args.environment, args.playbook, get_ansible_vault_password(), *unknown_args)
+            return ansible_playbook(args.environment, args.playbook, *unknown_args)
 
         def get_ansible_vault_password():
             if get_ansible_vault_password.value is None:
@@ -121,6 +148,29 @@ class UpdateConfig(object):
         AnsiblePlaybook.run(args, unknown_args)
 
 
+class BootstrapUsers(object):
+    command = 'bootstrap-users'
+    help = (
+        "Add users to a set of new machines as root. "
+        "This must be done before any other user can log in."
+    )
+
+    @staticmethod
+    def make_parser(parser):
+        arg_skip_check(parser)
+        arg_branch(parser)
+
+    @staticmethod
+    def run(args, unknown_args):
+        args.playbook = 'deploy_stack.yml'
+        public_vars = get_public_vars(args.environment)
+        root_user = public_vars.get('commcare_cloud_root_user', 'root')
+        unknown_args += ('--tags=users', '-u', root_user)
+        if not public_vars.get('commcare_cloud_pem'):
+            unknown_args += ('--ask-pass',)
+        AnsiblePlaybook.run(args, unknown_args)
+
+
 def git_branch():
     return subprocess.check_output("git branch | grep '^*' | cut -d' ' -f2", shell=True,
                                    cwd=os.path.expanduser('~/.commcare-cloud/ansible')).strip()
@@ -138,6 +188,7 @@ def check_branch(args):
 STANDARD_ARGS = [
     AnsiblePlaybook,
     UpdateConfig,
+    BootstrapUsers,
 ]
 
 
@@ -155,7 +206,7 @@ def main():
     subparsers = parser.add_subparsers(dest='command')
 
     for standard_arg in STANDARD_ARGS:
-        standard_arg.make_parser(subparsers.add_parser(subparsers.command, help=standard_arg.help))
+        standard_arg.make_parser(subparsers.add_parser(standard_arg.command, help=standard_arg.help))
 
     args, unknown_args = parser.parse_known_args()
     for standard_arg in STANDARD_ARGS:
