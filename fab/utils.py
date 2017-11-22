399bd0e50187af40a50ab572dce222d80e905bc0
Nick Pellegrino <npellegrino@dimagi.com>
2017-11-22 10:03:02 -0500

    Revert "update import"
    This reverts commit 0102755418fc152d6d53db3e85c5d92dd7dfba88.

--- fab/utils.py
+++ fab/utils.py
@@ -11,7 +11,7 @@ from fabric.api import local
 import re
 from getpass import getpass
 
-from github import Github
+from github3 import login
 from fabric.api import execute, env
 from fabric.colors import magenta
 
