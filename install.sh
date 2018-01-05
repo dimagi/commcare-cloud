08fd325332a34b5db1bd69c80d6303adc5e7d1d3
Daniel Roberts <droberts@dimagi.com>
2018-01-05 17:22:40 -0500

    make ./install.sh use requirements.txt

--- install.sh
+++ install.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-pip install pip --upgrade &
-
-pip install -e commcare-cloud &
-
-# to be able to run ansible commands
-pip install -r ansible/requirements.txt &
-
-# to be able to run ansible commands
-ansible-galaxy install -r ansible/requirements.yml &
-
-# to be able to run fab commands
-pip install -r fab/requirements.txt &
-
-wait
-
+pip install pip --upgrade
+pip install -r requirements.txt
+ansible-galaxy install -r ansible/requirements.yml
 ./control/check_install.sh
