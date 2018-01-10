7e690688d6d058fc40340d06901cb142a5a70f61
Daniel Miller <dmiller@dimagi.com>
2018-01-10 09:23:38 -0500

    Reference source

--- commcare-cloud/commcare_cloud/commcare_cloud.py
+++ commcare-cloud/commcare_cloud/commcare_cloud.py
@@ -568,6 +568,7 @@ class Mosh(Ssh):
 
 
 def git_branch():
+    # https://stackoverflow.com/a/19585361/10840
     return subprocess.check_output(
         "git status | head -1", cwd=ANSIBLE_DIR, shell=True
     ).strip().split()[-1]
