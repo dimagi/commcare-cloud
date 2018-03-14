8f65776b4049e5aaf8f5836b9adece66da29b4f9
Daniel Roberts <droberts@dimagi.com>
2018-03-14 17:54:15 -0400

    set up .travis.yml for running a test on the side
    that does't affect build outcome

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-test_syntax() {
-  ansible-playbook -i .travis/environments/travis/inventory.ini ansible/deploy_stack.yml --syntax-check
-}
+if [[ ${TEST} = 'main' ]]
+then
 
-test_localsettings() {
-  cp .travis/environments/travis/private.yml .travis/environments/travis/vault.yml
-  COMMCARE_CLOUD_ENVIRONMENTS=.travis/environments commcare-cloud travis deploy-stack --branch=FETCH_HEAD  --skip-check --quiet --tags=commcarehq
-  sudo python -m py_compile /home/cchq/www/travis/current/localsettings.py
-}
+    test_syntax() {
+        ansible-playbook -i .travis/environments/travis/inventory.ini ansible/deploy_stack.yml --syntax-check
+    }
 
-test_syntax
-test_localsettings
+    test_localsettings() {
+        cp .travis/environments/travis/private.yml .travis/environments/travis/vault.yml
+        COMMCARE_CLOUD_ENVIRONMENTS=.travis/environments commcare-cloud travis deploy-stack --branch=FETCH_HEAD  --skip-check --quiet --tags=commcarehq
+        sudo python -m py_compile /home/cchq/www/travis/current/localsettings.py
+    }
+
+    test_help_cache() {
+        diff <(ansible -h) commcare-cloud/commcare_cloud/help_cache/ansible.txt
+        diff <(ansible-playbook -h) commcare-cloud/commcare_cloud/help_cache/ansible-playbook.txt
+    }
+
+    test_syntax
+    test_localsettings
+    test_help_cache
+    nosetests
+
+elif [[ ${TEST} = 'prove-deploy' ]]
+then
+    fail() {
+        [[ 'a' = 'b' ]]
+    }
+fi
