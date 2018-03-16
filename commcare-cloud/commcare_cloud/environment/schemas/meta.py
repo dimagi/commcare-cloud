30f43c170f37398a16f1e75002c4b74712f12f5c
Daniel Roberts <droberts@dimagi.com>
2018-03-16 10:59:30 -0400

    rename env_name to env_monitoring_id
    to make it clearer how it differs from deploy_env

--- commcare-cloud/commcare_cloud/environment/schemas/meta.py
+++ commcare-cloud/commcare_cloud/environment/schemas/meta.py
@@ -4,4 +4,4 @@ import jsonobject
 class MetaConfig(jsonobject.JsonObject):
     _allow_dynamic_properties = False
     deploy_env = jsonobject.StringProperty(required=True)
-    env_name = jsonobject.StringProperty(required=True)
+    env_monitoring_id = jsonobject.StringProperty(required=True)
