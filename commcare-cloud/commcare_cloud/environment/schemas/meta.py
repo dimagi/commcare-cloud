4c35172b4d49fea4f3d2e7198b2f802a306749b3
Daniel Roberts <droberts@dimagi.com>
2018-03-16 10:33:29 -0400

    remove redundant `environment` setting
    always same (and semantically equivalent to) `deploy_env`

--- commcare-cloud/commcare_cloud/environment/schemas/meta.py
+++ commcare-cloud/commcare_cloud/environment/schemas/meta.py
@@ -3,6 +3,5 @@ import jsonobject
 
 class MetaConfig(jsonobject.JsonObject):
     _allow_dynamic_properties = False
-    environment = jsonobject.StringProperty(required=True)
     deploy_env = jsonobject.StringProperty(required=True)
     env_name = jsonobject.StringProperty(required=True)
