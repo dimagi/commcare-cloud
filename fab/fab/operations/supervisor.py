6d5ff1103e5981465e924ded100f2f27401ac768
Manish Kangia <Manish@Manishs-MacBook-Pro.local>
2018-03-21 14:37:11 +0530

    fix import

--- fab/fab/operations/supervisor.py
+++ fab/fab/operations/supervisor.py
@@ -7,7 +7,7 @@ import posixpath
 from contextlib import contextmanager
 import uuid
 
-from commcare_cloud.celery_utils import get_celery_worker_name
+from commcare_cloud.commands.celery_utils import get_celery_worker_name
 from commcare_cloud.commands.ansible.helpers import (
     get_django_webworker_name,
     get_formplayer_instance_name,
