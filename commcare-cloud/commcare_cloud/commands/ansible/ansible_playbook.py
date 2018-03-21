9badd856c9547c32281494ae4d1d2b91f1012df0
Daniel Roberts <droberts@dimagi.com>
2018-03-20 21:16:37 -0700

    fix weird password prompting for bootstrap-users
    by asking for vault password _before_ running ansible-playbook

--- commcare-cloud/commcare_cloud/commands/ansible/ansible_playbook.py
+++ commcare-cloud/commcare_cloud/commands/ansible/ansible_playbook.py
@@ -94,6 +94,7 @@ class AnsiblePlaybook(CommandBase):
             cmd_parts += get_common_ssh_args(public_vars)
             cmd = ' '.join(shlex_quote(arg) for arg in cmd_parts)
             print_command(cmd)
+            ansible_context.get_ansible_vault_password()
             p = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True, env=ansible_context.env_vars)
             if ask_vault_pass:
                 p.communicate(input='{}\n'.format(ansible_context.get_ansible_vault_password()))
