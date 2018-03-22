80d9f8b1a9b4b0f8101822c3e655ebce38364f0e
Simon Kelly <skelly@dimagi.com>
2018-03-22 10:35:28 +0200

    setup inter VM auth for Vagrant

--- provisioning/key_authorization.rb
+++ provisioning/key_authorization.rb
@@ -1,5 +1,4 @@
-def authorize_key_for_root(config, *key_paths)
-  [*key_paths, nil].each do |key_path|
+def authorize_key(config, key_path)
     if key_path.nil?
       fail "Public key not found at following paths: #{key_paths.join(', ')}"
     end
@@ -10,20 +9,14 @@ def authorize_key_for_root(config, *key_paths)
       config.vm.provision 'file',
         run: 'once',
         source: full_key_path,
-        destination: '/home/vagrant/root_pubkey'
+        destination: '/home/vagrant/.ssh/id_rsa'
 
       config.vm.provision 'shell',
         privileged: true,
         run: 'once',
         inline:
-          "echo \"Creating /root/.ssh/authorized_keys with #{key_path}\" && " +
-          'rm -f /root/.ssh/authorized_keys && ' +
-          'mv /home/vagrant/root_pubkey /root/.ssh/authorized_keys && ' +
-          'chown root:root /root/.ssh/authorized_keys && ' +
-          'chmod 600 /root/.ssh/authorized_keys && ' +
-          'rm -f /home/vagrant/root_pubkey && ' +
+          'chown vagrant:vagrant /home/vagrant/.ssh/id_rsa && ' +
+          'chmod 600 /home/vagrant/.ssh/id_rsa && ' +
           'echo "Done!"'
-      break
     end
-  end
 end
