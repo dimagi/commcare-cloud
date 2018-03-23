6785717b347bde0ebd884c5d40ed688998113556
Jonathan Emord <jonathan.emord@gmail.com>
2018-03-23 14:48:58 -0400

    Default deploy to a webworker

--- fab/fab/fabfile.py
+++ fab/fab/fabfile.py
@@ -253,7 +253,7 @@ def env_common():
     # if no server specified, just don't run pillowtop
     pillowtop = servers.get('pillowtop', [])
 
-    deploy = servers.get('deploy', servers['postgresql'])[:1]
+    deploy = servers.get('deploy', servers['webworkers'])[:1]
 
     env.roledefs = {
         'all': all,
