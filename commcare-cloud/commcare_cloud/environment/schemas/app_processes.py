c350e8c579fec8e234fb4afca248418a59039012
Daniel Roberts <droberts@dimagi.com>
2018-03-28 14:23:46 -0700

    fix pillowtop/celery config for bootstrap
    this changed because these are now done in ansible

--- commcare-cloud/commcare_cloud/environment/schemas/app_processes.py
+++ commcare-cloud/commcare_cloud/environment/schemas/app_processes.py
@@ -129,7 +129,7 @@ def check_and_translate_hosts(environment, host_mapping):
                 'Unable to translate host referenced '
                 'in app processes to a single host name: {}'.format(host))
             host_object = group_hosts[0]
-            host = host_object.get_vars().get('ansible_host', host_object.get_name())
+            host = host_object.get_name()
             translated[host] = config
 
     return translated
