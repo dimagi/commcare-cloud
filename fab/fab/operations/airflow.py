c2ee551fb5016a885cd070a474db03e7cd893692
Cal Ellowitz <calellowitz@gmail.com>
2018-03-29 16:09:08 +0530

    changing become user and fixing bad fab name

--- fab/fab/operations/airflow.py
+++ fab/fab/operations/airflow.py
@@ -6,7 +6,7 @@ from fabric.context_managers import cd
 from ..const import ROLES_AIRFLOW
 
 @roles(ROLES_AIRFLOW)
-def deploy_airflow():
+def update_airflow():
     with cd(env.airflow_code_root):
         sudo('git remote prune origin')
         sudo('git pull')
