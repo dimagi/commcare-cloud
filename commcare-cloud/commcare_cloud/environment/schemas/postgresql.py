c6ef67ca44d1c9325d448ac89330bffcd0295737
Daniel Roberts <droberts@dimagi.com>
2018-03-30 16:42:24 -0700

    push DEFAULT_POSTGRESQL_{USER,PASSWORD} to postgresql.yml

--- commcare-cloud/commcare_cloud/environment/schemas/postgresql.py
+++ commcare-cloud/commcare_cloud/environment/schemas/postgresql.py
@@ -5,6 +5,9 @@ class PostgresqlConfig(jsonobject.JsonObject):
     _allow_dynamic_properties = False
 
     DEFAULT_POSTGRESQL_HOST = jsonobject.StringProperty(default="{{ groups.postgresql.0 }}")
+    DEFAULT_POSTGRESQL_USER = jsonobject.StringProperty(default="{{ secrets.POSTGRES_USERS.commcare.username }}")
+    DEFAULT_POSTGRESQL_PASSWORD = jsonobject.StringProperty(default="{{ secrets.POSTGRES_USERS.commcare.password }}")
+
     postgresql_dbs = jsonobject.ListProperty(lambda: DBOptions, required=True)
 
     @classmethod
@@ -13,6 +16,10 @@ class PostgresqlConfig(jsonobject.JsonObject):
         for db in self.postgresql_dbs:
             if not db.host:
                 db.host = self.DEFAULT_POSTGRESQL_HOST
+            if not db.user:
+                db.user = self.DEFAULT_POSTGRESQL_USER
+            if not db.password:
+                db.password = self.DEFAULT_POSTGRESQL_PASSWORD
         return self
 
 
@@ -22,8 +29,8 @@ class DBOptions(jsonobject.JsonObject):
     name = jsonobject.StringProperty(exclude_if_none=True)
     host = jsonobject.StringProperty()
     port = jsonobject.IntegerProperty(default=6432)
-    user = jsonobject.StringProperty(exclude_if_none=True)
-    password = jsonobject.StringProperty(exclude_if_none=True)
+    user = jsonobject.StringProperty()
+    password = jsonobject.StringProperty()
     options = jsonobject.DictProperty(unicode, exclude_if_none=True)
     django_alias = jsonobject.StringProperty(exclude_if_none=True)
     shards = jsonobject.ListProperty(int, exclude_if_none=True)  # [start, end] pair
