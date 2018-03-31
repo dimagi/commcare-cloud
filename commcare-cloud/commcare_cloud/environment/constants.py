ccfb0e3fb9bec587ceedd47f431e04a8e259a090
Daniel Roberts <droberts@dimagi.com>
2018-03-30 17:35:07 -0700

    make postgresql_db_name a shared constant

--- commcare-cloud/commcare_cloud/environment/constants.py
+++ commcare-cloud/commcare_cloud/environment/constants.py
@@ -3,6 +3,7 @@ import jsonobject
 
 class _Constants(jsonobject.JsonObject):
     formplayer_db_name = jsonobject.StringProperty(default='formplayer')
+    postgresql_db_name = jsonobject.StringProperty(default='commcarehq')
 
 
 constants = _Constants()
