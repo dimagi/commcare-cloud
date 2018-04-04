ca02dd20127e5a56ddc60c34dec05c07f92d927b
Daniel Roberts <droberts@dimagi.com>
2018-04-03 19:12:29 -0700

    rename var to commcarehq_main_db_name

--- commcare-cloud/commcare_cloud/environment/constants.py
+++ commcare-cloud/commcare_cloud/environment/constants.py
@@ -3,7 +3,7 @@ import jsonobject
 
 class _Constants(jsonobject.JsonObject):
     formplayer_db_name = jsonobject.StringProperty(default='formplayer')
-    postgresql_db_name = jsonobject.StringProperty(default='commcarehq')
+    commcarehq_main_db_name = jsonobject.StringProperty(default='commcarehq')
 
 
 constants = _Constants()
