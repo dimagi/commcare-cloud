2d6eb244b09ea63d7f6088f73c31635b3e5aabb6
Daniel Roberts <droberts@dimagi.com>
2018-04-04 15:06:49 -0700

    add postgresql smart `dbs` config
    - currently just auto-generates the known pieces of the main db config
    - lets you list `custom` dbs (currently all other dbs)
    - currently compares what it generates against the static postgresql_dbs var in tests
      as scaffolding to reduce (my) human error in development

--- commcare-cloud/commcare_cloud/environment/schemas/postgresql.py
+++ commcare-cloud/commcare_cloud/environment/schemas/postgresql.py
@@ -1,4 +1,7 @@
 import jsonobject
+import re
+
+from commcare_cloud.environment.constants import constants
 
 
 class PostgresqlConfig(jsonobject.JsonObject):
@@ -10,12 +13,17 @@ class PostgresqlConfig(jsonobject.JsonObject):
     DEFAULT_POSTGRESQL_PASSWORD = jsonobject.StringProperty(default="{{ secrets.POSTGRES_USERS.commcare.password }}")
     REPORTING_DATABASES = jsonobject.DictProperty(default=lambda: {"ucr": "ucr"})
 
-    postgresql_dbs = jsonobject.ListProperty(lambda: DBOptions, required=True)
+    postgresql_dbs = jsonobject.ListProperty(lambda: ShardDBOptions, required=True)
+    dbs = jsonobject.ObjectProperty(lambda: SmartDBConfig, required=True)
+
+    @property
+    def all_dbs(self):
+        return self.postgresql_dbs + self.generate_postgresql_dbs()
 
     @classmethod
     def wrap(cls, data):
         self = super(PostgresqlConfig, cls).wrap(data)
-        for db in self.postgresql_dbs:
+        for db in self.all_dbs:
             if not db.user:
                 db.user = self.DEFAULT_POSTGRESQL_USER
             if not db.password:
@@ -29,11 +37,31 @@ class PostgresqlConfig(jsonobject.JsonObject):
             self.DEFAULT_POSTGRESQL_HOST = environment.translate_host(
                 self.DEFAULT_POSTGRESQL_HOST, environment.paths.postgresql_yml)
 
-        for db in self.postgresql_dbs:
+        for db in self.all_dbs:
             if db.host is None:
                 db.host = self.DEFAULT_POSTGRESQL_HOST
             elif db.host != '127.0.0.1':
                 db.host = environment.translate_host(db.host, environment.paths.postgresql_yml)
+
+    def generate_postgresql_dbs(self):
+        return [self.dbs.main] + self.dbs.custom
+
+    def check(self):
+        def get_normalized(db_list):
+            return sorted([db.to_json() for db in db_list], key=lambda db: db['name'])
+
+        assert get_normalized(self.generate_postgresql_dbs()) == \
+            get_normalized(self.postgresql_dbs), \
+            "{} != {}".format(get_normalized(self.generate_postgresql_dbs()),
+                              get_normalized(self.postgresql_dbs))
+
+
+class SmartDBConfig(jsonobject.JsonObject):
+    _allow_dynamic_properties = False
+
+    main = jsonobject.ObjectProperty(lambda: MainDBOptions)
+
+    custom = jsonobject.ListProperty(lambda: ShardDBOptions)
 
 
 class DBOptions(jsonobject.JsonObject):
@@ -46,7 +74,22 @@ class DBOptions(jsonobject.JsonObject):
     password = jsonobject.StringProperty()
     options = jsonobject.DictProperty(unicode)
     django_alias = jsonobject.StringProperty()
-    shards = jsonobject.ListProperty(int, exclude_if_none=True)  # [start, end] pair
     django_migrate = jsonobject.BooleanProperty(default=True)
     query_stats = jsonobject.BooleanProperty(default=False)
     create = jsonobject.BooleanProperty(default=True)
+
+    @classmethod
+    def wrap(cls, data):
+        self = super(DBOptions, cls).wrap(data)
+        if re.match('^{{\s*commcarehq_main_db_name\s*}}$', self.name):
+            self.name = constants.commcarehq_main_db_name
+        return self
+
+
+class MainDBOptions(DBOptions):
+    name = constants.commcarehq_main_db_name
+    django_alias = 'default'
+
+
+class ShardDBOptions(DBOptions):
+    shards = jsonobject.ListProperty(int, exclude_if_none=True)  # [start, end] pair
