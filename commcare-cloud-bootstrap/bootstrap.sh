516ea40ae131552538aa3520f922759ecce46512
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-04-17 16:35:25 -0400

    Adding --become-user ubuntu to deploy-stack

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -5,12 +5,15 @@ BRANCH=$2
 SPEC=$3
 
 #commcare-cloud-bootstrap provision $SPEC --env $ENV
+#
+#sleep 5 # Wait 5 seconds to let the servers initialize.
+#
 #while
 #    commcare-cloud $ENV run-shell-command all 'echo {{ inventory_hostname }}' -u ubuntu
 #    [ $? = 4 ]
 #do :
-#done
+#    done
 
-commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
-#commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
+#commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
+commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
 #commcare-cloud $ENV fab deploy:confirm=no
