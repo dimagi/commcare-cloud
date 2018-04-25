80e74f777e29754779ae5556aed4d0df6e9631a2
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-04-25 15:33:57 -0400

    Cleanup

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -15,5 +15,5 @@ commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
 commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
 
 ## This next line is a temp fix.
-commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check --start-at-task="find supervisord path" --tags="supervisor"
+commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check
 commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
