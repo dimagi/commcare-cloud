c0f16cb2d8e59109197e477c053aebeb7c5d1132
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-04-25 14:33:38 -0400

    Correctly set which_supervisord variable if it has to get installed first

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -14,6 +14,6 @@ done
 commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
 commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
 
- This next line is a temp fix.
-commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check
+## This next line is a temp fix.
+commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check --start-at-task="find supervisord path" --tags="supervisor"
 commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
