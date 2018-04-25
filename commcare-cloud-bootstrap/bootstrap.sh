62cefabff7cc797e6a6c42429e9495d4d1169311
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-04-25 19:08:04 -0400

    Update bootstrap.sh comment

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -14,6 +14,6 @@ done
 commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
 commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
 
-## This next line is a temp fix.
+## This next line is a temp fix to allow supervisor to actually get installed.
 commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check
 commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
