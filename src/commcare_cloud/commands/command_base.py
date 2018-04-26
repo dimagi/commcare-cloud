4217820a79210823bdcba9f5d678df26d0a65b1d
Daniel Roberts <droberts@dimagi.com>
2018-04-26 14:17:49 -0400

    add option to define arguments statically

--- src/commcare_cloud/commands/command_base.py
+++ src/commcare_cloud/commands/command_base.py
@@ -7,14 +7,24 @@ class CommandBase(object):
     command = None
     help = None
     aliases = ()
+    arguments = ()
 
     def __init__(self, parser):
         self.parser = parser
 
-    @abc.abstractmethod
     def make_parser(self):
-        pass
+        for argument in self.arguments:
+            argument.add_to_parser(self.parser)
 
     @abc.abstractmethod
     def run(self, args, unknown_args):
         pass
+
+
+class Argument(object):
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+
+    def add_to_parser(self, parser):
+        parser.add_argument(*self._args, **self._kwargs)
--- src/commcare_cloud/commands/shared_args.py
+++ src/commcare_cloud/commands/shared_args.py
@@ -1,40 +1,54 @@
 import os
 
+from commcare_cloud.commands.command_base import Argument
+
+INVENTORY_GROUP_ARG = Argument('inventory_group', help=(
+    "The inventory group to run the command on. Use 'all' for all hosts."
+))
+
+SKIP_CHECK_ARG = Argument('--skip-check', action='store_true', default=False, help=(
+    "skip the default of viewing --check output first"
+))
+
+QUIET_ARG = Argument('--quiet', action='store_true', default=False, help=(
+    "skip all user prompts and proceed as if answered in the affirmative"
+))
+
+BRANCH_ARG = Argument('--branch', default='master', help=(
+    "the name of the commcare-cloud git branch to run against, if not master"
+))
+
+STDOUT_CALLBACK_ARG = Argument(
+    '--output', dest='stdout_callback', choices=['actionable', 'minimal'],
+    default=os.environ.get('ANSIBLE_STDOUT_CALLBACK') or 'default',
+    help=("The callback plugin to use for generating output. "
+          "See ansible-doc -t callback -l and ansible-doc -t callback [ansible|minimal]")
+)
+
+LIMIT_ARG = Argument('-l', '--limit', metavar="SUBSET", help=(
+    "further limit selected hosts to an additional pattern"
+))
+
 
 def arg_inventory_group(parser):
-    parser.add_argument('inventory_group', help=(
-        "The inventory group to run the command on. Use 'all' for all hosts."
-    ))
+    INVENTORY_GROUP_ARG.add_to_parser(parser)
 
 
 def arg_skip_check(parser):
-    parser.add_argument('--skip-check', action='store_true', default=False, help=(
-        "skip the default of viewing --check output first"
-    ))
+    SKIP_CHECK_ARG.add_to_parser(parser)
 
 
 def arg_quiet(parser):
-    parser.add_argument('--quiet', action='store_true', default=False, help=(
-        "skip all user prompts and proceed as if answered in the affirmative"
-    ))
+    QUIET_ARG.add_to_parser(parser)
 
 
 def arg_branch(parser):
-    parser.add_argument('--branch', default='master', help=(
-        "the name of the commcare-cloud git branch to run against, if not master"
-    ))
+    BRANCH_ARG.add_to_parser(parser)
 
 
 def arg_stdout_callback(parser):
-    default = os.environ.get('ANSIBLE_STDOUT_CALLBACK') or 'default'
-    parser.add_argument(
-        '--output', dest='stdout_callback', default=default, choices=['actionable', 'minimal'],
-        help=("The callback plugin to use for generating output. "
-              "See ansible-doc -t callback -l and ansible-doc -t callback [ansible|minimal]")
-    )
+    STDOUT_CALLBACK_ARG.add_to_parser(parser)
 
 
 def arg_limit(parser):
-    parser.add_argument('-l', '--limit', metavar="SUBSET", help=(
-        "further limit selected hosts to an additional pattern"
-    ))
+    LIMIT_ARG.add_to_parser(parser)
