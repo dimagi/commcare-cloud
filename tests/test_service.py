5c9e8c7f399105eac070893abaa990c7b348ddc5
Simon Kelly <skelly@dimagi.com>
2018-04-26 14:43:15 +0200

    test for get_processes_by_host

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 
+from nose_parameterized import parameterized
+
 from commcare_cloud.commands.ansible.helpers import ProcessDescriptor
-from commcare_cloud.commands.ansible.service import get_managed_service_options
+from commcare_cloud.commands.ansible.service import get_managed_service_options, get_processes_by_host
 
 process_descriptors = [
         ProcessDescriptor('h1', 'p1', 0, 'p1-0'),
@@ -9,6 +11,7 @@ process_descriptors = [
         ProcessDescriptor('h2', 'p1', 2, 'p1-2'),
         ProcessDescriptor('h1', 'p2', 0, 'p2-0'),
         ProcessDescriptor('h3', 'p3', 0, 'p3-0'),
+        ProcessDescriptor('h4', 'p3', 0, 'p3-0'),
     ]
 
 
@@ -19,3 +22,21 @@ def test_get_managed_service_options():
         'p2',
         'p3',
     ], options
+
+
+@parameterized([
+    (['h1', 'h2', 'h3', 'h4'], process_descriptors, None)
+])
+def test_get_processes_by_host(all_hosts, process_descriptors, process_pattern):
+    processes_by_host = get_processes_by_host(all_hosts, process_descriptors, process_pattern)
+
+    # sort for comparison
+    processes_by_host = {
+        hosts: sorted(process)
+        for hosts, process in processes_by_host.items()
+    }
+    assert processes_by_host == {
+        ('h1',): ['p1-0', 'p1-1', 'p2-0'],
+        ('h2',): ['p1-2'],
+        ('h3','h4'): ['p3-0'],
+    }
