df72dac39108ed8e17908afd3655f706b728bec6
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-05-10 12:45:28 +0530

    Set user as root user if factory auth is being invoked

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -12,8 +12,8 @@ do :
 done
 
 commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
-#commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
-#
-### This next line is a temp fix to allow supervisor to actually get installed.
-#commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check
-#commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
+commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
+
+## This next line is a temp fix to allow supervisor to actually get installed.
+commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check
+commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
