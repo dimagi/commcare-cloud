6b225212d4e2849ffadc77a55a1eae0a71c558eb
Simon Kelly <skelly@dimagi.com>
2018-05-11 16:12:02 +0200

    trip whitespace

--- tests/migration_config/plans/reshard/expected_couchdb_rsync.sh
+++ tests/migration_config/plans/reshard/expected_couchdb_rsync.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 rsync -e 'ssh -oStrictHostKeyChecking=no' --append-verify -aH --info=progress2 \
     ansible@10.247.164.12:/opt/data/couchdb2/ /opt/data/couchdb2/ \
     --files-from /tmp/couchdb_migration_rsync_file_list/10.247.164.12__files -r $@
