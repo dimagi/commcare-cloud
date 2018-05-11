209b574e5ed6f786f314c5dad5c040f57c88d255
Simon Kelly <skelly@dimagi.com>
2018-05-11 11:39:54 +0200

    enforce hostnames for IP hosts

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+from ipaddress import ip_address
 from parameterized import parameterized
 
 from commcare_cloud.environment.main import get_environment
@@ -23,3 +24,20 @@ def test_fab_settings_yml(env):
 def test_all(env):
     environment = get_environment(env)
     environment.check()
+
+
+@parameterized(get_available_envs())
+def test_hostnames(env):
+    environment = get_environment(env)
+    missing_hostnames = set()
+    for group, hosts in environment.sshable_hostnames_by_group.items():
+        for host in hosts:
+            try:
+                ip_address(host)
+            except ValueError:
+                pass
+            else:
+                hostname = environment.get_hostname(host)
+                if hostname == host:
+                    missing_hostnames.add(host)
+    assert len(missing_hostnames) == 0, "Envornment hosts missing hostnames".format(list(missing_hostnames))
