508face9dc14fd13c6fbd1f119a63487054c2468
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-05-18 07:36:51 -0400

    Get rid of failed when: no in main.yml, change the temp fix in bootstrap.sh to actually rerun supervisor commands

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -15,5 +15,5 @@ commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
 commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
 
 ## This next line is a temp fix to allow supervisor to actually get installed.
-commcare-cloud $ENV update-users --quiet --branch=$BRANCH --skip-check
+commcare-cloud $ENV deploy-stack --tags=supervisor --quiet --branch=$BRANCH --skip-check
 commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
