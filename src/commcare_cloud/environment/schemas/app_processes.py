0645a8e5c9e907e8659119f94c60370d6ba1be8e
Jonathan Emord <jonathan.emord@gmail.com>
2018-05-31 23:23:18 -0400

    Decouple beat from periodic queue

--- src/commcare_cloud/environment/schemas/app_processes.py
+++ src/commcare_cloud/environment/schemas/app_processes.py
@@ -66,6 +66,7 @@ class CeleryProcess(namedtuple('CeleryProcess', ['name', 'required'])):
 CELERY_PROCESSES = [
     CeleryProcess("async_restore_queue"),
     CeleryProcess("background_queue"),
+    CeleryProcess("beat", required=False),
     CeleryProcess("case_rule_queue"),
     CeleryProcess("celery"),
     CeleryProcess("celery_periodic", required=False),
