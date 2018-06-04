dea77acf4c09ce32d92be4ba3f98149543176218
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-06-04 13:05:51 -0400

    Assert that each element in the list must be unicode

--- src/commcare_cloud/environment/schemas/meta.py
+++ src/commcare_cloud/environment/schemas/meta.py
@@ -5,5 +5,5 @@ class MetaConfig(jsonobject.JsonObject):
     _allow_dynamic_properties = False
     deploy_env = jsonobject.StringProperty(required=True)
     env_monitoring_id = jsonobject.StringProperty(required=True)
-    users = jsonobject.ListProperty(required=True)
+    users = jsonobject.ListProperty(unicode, required=True)
     slack_alerts_channel = jsonobject.StringProperty()
