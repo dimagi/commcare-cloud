98dac6c6d518c11e48bab6fd471f043c211d8ca5
Simon Kelly <skelly@dimagi.com>
2018-06-07 16:43:57 +0200

    replace expanded home location with tilde

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -17,8 +17,8 @@ then
     }
 
     test_help_cache() {
-        diff <(ansible -h) src/commcare_cloud/help_cache/ansible.txt
-        diff <(ansible-playbook -h) src/commcare_cloud/help_cache/ansible-playbook.txt
+        diff <(ansible -h | sed "s#$HOME#\~#g") src/commcare_cloud/help_cache/ansible.txt
+        diff <(ansible-playbook -h | sed "s#$HOME#\~#g") src/commcare_cloud/help_cache/ansible-playbook.txt
     }
 
     test_syntax
