a6684ab9d1cda42414be1969a971ef9a5f55e1d6
Dimagi <dimagi@prettythings.g2techgroup.net>
2018-06-26 15:34:22 +0000

    Added Airflow, PG_Proxy, and Touchforms servers. Added ElastiCache Redis setup

--- variables.tf
+++ variables.tf
@@ -53,20 +53,6 @@ module "network" {
   #openvpn-access-sg = "${module.openvpn.openvpn-access-sg}"
 }
 
-#module "shared-alb" {
-#  source = "../modules/shared-alb"
-#  environment           = "${var.environment}"
-#  vpc_id                = "${module.network.vpc-id}"
-#  lb_subnets            = ["${module.network.subnet-a-public}", "${module.network.subnet-b-public}", "${module.network.subnet-c-public}"]
-#  g2-access-sg          = "${module.network.g2-access-sg}"
-#  openvpn-access-sg     = "${module.openvpn.openvpn-access-sg}"
-#  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
-#  jenkins_tg            = "${module.jenkins.jenkins_tg}"
-  # dns_zone_id           = "${var.dns_zone_id}"
-  # dns_domain            = "${var.dns_domain}"
-  # internal_ssl_cert_arn = "${var.internal_ssl_cert_arn}"
-#}
-
 module "django" {
   source                = "../modules/server"
   server_name		= "django"
@@ -151,83 +137,123 @@ module "ES" {
   vpc_id                = "${module.network.vpc-id}"
 }
 
-#module "celery" {
-#  source                = "../modules/server"
-#  server_image       = "${var.server_image}"
-#  environment           = "${var.environment}"
-#  company           = "${var.company}"
-#  server_instance_type = "${var.server_instance_type}"
-#  g2-access-sg          = "${module.network.g2-access-sg}"
-  #openvpn-access-sg     = "${module.openvpn.openvpn-access-sg}"
-#  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
-#  instance_subnet       = "${module.network.subnet-c-public}"
-#  vpc_id                = "${module.network.vpc-id}"
- # lb_subnets            = ["${module.network.subnet-a-public}", "${module.network.subnet-b-public}", "${module.network.subnet-c-public}"]
-  # dns_zone_id           = "${var.dns_zone_id}"
-  # dns_domain            = "${var.dns_domain}"
-  # shared-alb-dns-name   = "${module.shared-alb.alb-dns-name}"
-#}
+module "Airflow" {
+  source                = "../modules/server"
+  server_name           = "Airflow"
+  server_image          = "${var.server_image}"
+  environment           = "${var.environment}"
+  company               = "${var.company}"
+  server_instance_type  = "${var.server_instance_type}"
+  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
+  instance_subnet       = "${module.network.subnet-a-app-private}"
+  vpc_id                = "${module.network.vpc-id}"
+}
+
+module "Touchforms" {
+  source                = "../modules/server"
+  server_name           = "Touchforms"
+  server_image          = "${var.server_image}"
+  environment           = "${var.environment}"
+  company               = "${var.company}"
+  server_instance_type  = "${var.server_instance_type}"
+  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
+  instance_subnet       = "${module.network.subnet-a-app-private}"
+  vpc_id                = "${module.network.vpc-id}"
+}
+
+module "RabbitMQ" {
+  source                = "../modules/server"
+  server_name           = "RabbitMQ"
+  server_image          = "${var.server_image}"
+  environment           = "${var.environment}"
+  company               = "${var.company}"
+  server_instance_type  = "${var.server_instance_type}"
+  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
+  instance_subnet       = "${module.network.subnet-a-app-private}"
+  vpc_id                = "${module.network.vpc-id}"
+}
+
+module "PG_Proxy" {
+  source                = "../modules/server"
+  server_name           = "PG_Proxy"
+  server_image          = "${var.server_image}"
+  environment           = "${var.environment}"
+  company               = "${var.company}"
+  server_instance_type  = "${var.server_instance_type}"
+  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
+  instance_subnet       = "${module.network.subnet-a-app-private}"
+  vpc_id                = "${module.network.vpc-id}"
+}
+
+module "Redis" {
+  source               = "../modules/elasticache"
+  cluster_id           = "${var.environment}-redis"
+  engine               = "redis"
+  engine_version       = "${var.engine_version}"
+  node_type            = "${var.redis_node_type}"
+  num_cache_nodes      = "${var.num_redis_nodes}"
+  parameter_group_name = "${var.parameter_group_name}"
+  port                 = 6379
+  elasticache_subnets  = ["${module.network.subnet-a-util-private}","${module.network.subnet-b-util-private}","${module.network.subnet-c-util-private}"]
+  security_group_ids   = ["${module.network.vpc-all-hosts-sg}"]
+}
 
 #module "bastion" {
 #  source                = "../modules/bastion"
-#  bastion_image       = "${var.bastion_image}"
+#  bastion_image         = "${var.bastion_image}"
 #  environment           = "${var.environment}"
-#  company           = "${var.company}"
+#  company               = "${var.company}"
 #  bastion_instance_type = "${var.bastion_instance_type}"
 #  g2-access-sg          = "${module.network.g2-access-sg}"
 #  #openvpn-access-sg     = "${module.openvpn.openvpn-access-sg}"
 #  vpc-all-hosts-sg      = "${module.network.vpc-all-hosts-sg}"
 #  instance_subnet       = "${module.network.subnet-c-public}"
 #  vpc_id                = "${module.network.vpc-id}"
-# # lb_subnets            = ["${module.network.subnet-a-public}", "${module.network.subnet-b-public}", "${module.network.subnet-c-public}"]
-#  # dns_zone_id           = "${var.dns_zone_id}"
-#  # dns_domain            = "${var.dns_domain}"
-#  # shared-alb-dns-name   = "${module.shared-alb.alb-dns-name}"
 #}
 
 #module "openvpn" {
-#  source              = "../modules/openvpn"
-#  openvpn_image       = "${var.openvpn_image}"
-#  environment         = "${var.environment}"
-#  company           = "${var.company}"  
-#  vpn_size            = "${var.openvpn_instance_type}"
-#  g2-access-sg        = "${module.network.g2-access-sg}"
-#  vpc-all-hosts-sg    = "${module.network.vpc-all-hosts-sg}"
-#  instance_subnet     = "${module.network.subnet-b-public}"
-#  vpc_id              = "${module.network.vpc-id}"
-#  # dns_zone_id         = "${var.dns_zone_id}"
-#  # dns_domain          = "${var.dns_domain}"
+#  source           = "../modules/openvpn"
+#  openvpn_image    = "${var.openvpn_image}"
+#  environment      = "${var.environment}"
+#  company          = "${var.company}"  
+#  vpn_size         = "${var.openvpn_instance_type}"
+#  g2-access-sg     = "${module.network.g2-access-sg}"
+#  vpc-all-hosts-sg = "${module.network.vpc-all-hosts-sg}"
+#  instance_subnet  = "${module.network.subnet-b-public}"
+#  vpc_id           = "${module.network.vpc-id}"
+#  # dns_zone_id      = "${var.dns_zone_id}"
+#  # dns_domain       = "${var.dns_domain}"
 #}
 
 # If an RDS instance or cluster is needed, uncomment this.
-# module "database" {
-#   source                         = "../modules/rds"
-#   vpc_id                         = "${module.network.vpc-id}"
-#   environment                    = "${var.environment}"
-#   dns_zone_id                    = "${var.dns_zone_id}"
-#   dns_domain                     = "${var.dns_domain}"
-#   azs                            = "${var.azs}"
-#   rds_storage                    = "${var.rds_storage}"
-#   rds_engine                     = "${var.rds_engine}"
-#   rds_engine_version             = "${var.rds_engine_version}"
-#   rds_instance_type              = "${var.rds_instance_type}"
-#   rds_database_name              = "${var.rds_database_name}"
-#   rds_username                   = "${var.rds_username}"
-#   rds_storage_type               = "${var.rds_storage_type}"
-#   rds_backup_window              = "${var.rds_backup_window}"
-#   rds_backup_retention           = "${var.rds_backup_retention}"
-#   rds_maintenance_window         = "${var.rds_maintenance_window}"
-#   rds_auto_minor_version_upgrade = "${var.rds_auto_minor_version_upgrade}"
-#   rds_multi_az                   = "${var.rds_multi_az}"
-#   rds_storage_encrypted          = "${var.rds_storage_encrypted}"
-#   rds_port                       = "${var.rds_port}"
-#   environment                    = "${var.environment}"
-#   rds_password                   = "${var.rds_password}"
-#   rds_prevent_destroy            = "${var.rds_prevent_destroy}"
-#   rds_instance_count             = "${var.rds_instance_count}"
-#   rds_sg_ip_ingress              = ["${module.network.vpc-all-hosts-sg}","${module.network.g2-access-sg}","${module.openvpn.openvpn-access-sg}"]
-#   rds_subnet_a                   = "${module.network.subnet-a-db-private}"
-#   rds_subnet_b                   = "${module.network.subnet-b-db-private}"
-#   rds_subnet_c                   = "${module.network.subnet-c-db-private}"
-#   rds_instance_count             = "${var.rds_instance_count}"
-# }
+#module "database" {
+#  source                         = "../modules/rds"
+#  vpc_id                         = "${module.network.vpc-id}"
+#  environment                    = "${var.environment}"
+#  dns_zone_id                    = "${var.dns_zone_id}"
+#  dns_domain                     = "${var.dns_domain}"
+#  azs                            = "${var.azs}"
+#  rds_storage                    = "${var.rds_storage}"
+#  rds_engine                     = "${var.rds_engine}"
+#  rds_engine_version             = "${var.rds_engine_version}"
+#  rds_instance_type              = "${var.rds_instance_type}"
+#  rds_database_name              = "${var.rds_database_name}"
+#  rds_username                   = "${var.rds_username}"
+#  rds_storage_type               = "${var.rds_storage_type}"
+#  rds_backup_window              = "${var.rds_backup_window}"
+#  rds_backup_retention           = "${var.rds_backup_retention}"
+#  rds_maintenance_window         = "${var.rds_maintenance_window}"
+#  rds_auto_minor_version_upgrade = "${var.rds_auto_minor_version_upgrade}"
+#  rds_multi_az                   = "${var.rds_multi_az}"
+#  rds_storage_encrypted          = "${var.rds_storage_encrypted}"
+#  rds_port                       = "${var.rds_port}"
+#  environment                    = "${var.environment}"
+#  rds_password                   = "${var.rds_password}"
+#  rds_prevent_destroy            = "${var.rds_prevent_destroy}"
+#  rds_instance_count             = "${var.rds_instance_count}"
+#  rds_sg_ip_ingress              = ["${module.network.vpc-all-hosts-sg}","${module.network.g2-access-sg}","${module.openvpn.openvpn-access-sg}"]
+#  rds_subnet_a                   = "${module.network.subnet-a-db-private}"
+#  rds_subnet_b                   = "${module.network.subnet-b-db-private}"
+#  rds_subnet_c                   = "${module.network.subnet-c-db-private}"
+#  rds_instance_count             = "${var.rds_instance_count}"
+#}
