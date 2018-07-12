d8fbaab19b547b82eb75e6f4bfdca9df6be3dd89
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-07-12 13:30:11 -0700

    Test deploying this on travis [pv-deploy]

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -4,14 +4,14 @@ ENV=$1
 BRANCH=$2
 SPEC=$3
 
-commcare-cloud-bootstrap provision $SPEC --env $ENV
-while
-    commcare-cloud $ENV ping all --use-factory-auth
-    [ $? = 4 ]
-do :
-done
-
-commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
-commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH
+#commcare-cloud-bootstrap provision $SPEC --env $ENV
+#while
+#    commcare-cloud $ENV ping all --use-factory-auth
+#    [ $? = 4 ]
+#do :
+#done
+#
+#commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
+commcare-cloud $ENV deploy-stack --skip-check --quiet -e 'CCHQ_IS_FRESH_INSTALL=1' --branch=$BRANCH --start-at-task="Install NFS packages"
 
 commcare-cloud $ENV fab deploy:confirm=no --show=debug --set ignore_kafka_checkpoint_warning=true
