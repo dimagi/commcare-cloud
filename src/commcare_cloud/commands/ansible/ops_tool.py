c2463459abee8a09d0b423ed612d2cf1196c9cd7
Nitigya Sharma <nitigyasharma@hotmail.com>
2018-08-13 18:22:42 +0530

    Done proposed changes

--- src/commcare_cloud/commands/ansible/ops_tool.py
+++ src/commcare_cloud/commands/ansible/ops_tool.py
@@ -9,7 +9,7 @@ class PrivilegedCommands():
     """
     This Class allows to execute sudo commands over ssh.
     """
-    def __init__(self, user_name, password, known_host_file, privleged_command):
+    def __init__(self, user_name, password, known_host_file, privleged_command, user_as):
         """
 
         :param user_name: Username to login with
@@ -17,26 +17,25 @@ class PrivilegedCommands():
         :param known_host_file: Path to the known host file
         :param privleged_command: command to execute (This command will be executed using sudo. )
         """
+
         self.user_name = user_name
         self.password = password
-        self.privileged_command = privleged_command if privleged_command.startswith('sudo') \
-            else 'sudo ' + privleged_command
+        self.privileged_command = privleged_command
         self.known_host_file = known_host_file
+        self.user_as = user_as
 
     def run_parallel_command(self, hosts):
         from fabric.api import execute, sudo, env
         if env.ssh_config_path and os.path.isfile(os.path.expanduser(env.ssh_config_path)):
             env.use_ssh_config = True
         env.forward_agent = True
-        # pass `-E` to sudo to preserve environment for ssh agent forwarding
-        env.sudo_prefix = "sudo -SE -p '%(sudo_prompt)s' "
         env.user = self.user_name
         env.password = self.password
         env.hosts = hosts
         env.warn_only = True
 
         def _task():
-            result = sudo(self.privileged_command)
+            result = sudo(self.privileged_command, user=self.user_as )
             return result
 
         res = execute(_task)
@@ -86,14 +85,15 @@ class ListDatabases(CommandBase):
         dbs_present_in_host = collections.defaultdict(list)
         args.server = 'postgresql'
         ansible_username = 'ansible'
-        command = "sudo -iu postgres python /usr/local/sbin/db-tools.py  --list-all"
+        command = "python /usr/local/sbin/db-tools.py  --list-all"
 
         environment = get_environment(args.env_name)
         ansible_password = environment.get_ansible_user_password()
         host_addresses = get_instance_group(args.env_name, args.server)
         known_host_file = environment.paths.known_hosts
+        user_as = 'postgres'
 
-        privileged_command = PrivilegedCommands(ansible_username, ansible_password, known_host_file, command)
+        privileged_command = PrivilegedCommands(ansible_username, ansible_password, known_host_file, command, user_as)
 
         present_db_op = privileged_command.run_parallel_command(host_addresses)
 
