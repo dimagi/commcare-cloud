38a99053372487e2bd2dd93e25dfe90918baf742
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-08-13 12:42:07 -0400

    Put all tags in a variable

--- commcare-cloud-bootstrap/bootstrap.sh
+++ commcare-cloud-bootstrap/bootstrap.sh
@@ -4,6 +4,8 @@ ENV=$1
 BRANCH=$2
 SPEC=$3
 
+PLAYBOOK_TAGS="--skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'"
+
 commcare-cloud-bootstrap provision $SPEC --env $ENV
 while
     commcare-cloud $ENV ping all --use-factory-auth
@@ -13,24 +15,24 @@ done
 
 commcare-cloud $ENV bootstrap-users --quiet --branch=$BRANCH
 
-commcare-cloud $ENV ansible-playbook update_apt_cache.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_common.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_lvm.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_db.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_commcarehq.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_proxy.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_shared_dir.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_webworker.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_formplayer.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_mailrelay.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_tmpreaper.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_etckeeper.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_airflow.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
+commcare-cloud $ENV ansible-playbook update_apt_cache.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_common.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_lvm.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_db.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_commcarehq.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_proxy.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_shared_dir.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_webworker.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_formplayer.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_mailrelay.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_tmpreaper.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_etckeeper.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_airflow.yml $PLAYBOOK_TAGS
 # migrate must happen before first fab, and also before touchforms can create its special user
 
-commcare-cloud $ENV ansible-playbook migrate_on_fresh_install.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_touchforms.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
-commcare-cloud $ENV ansible-playbook deploy_http_proxy.yml --skip-check --quiet --branch=$BRANCH -e 'CCHQ_IS_FRESH_INSTALL=1'
+commcare-cloud $ENV ansible-playbook migrate_on_fresh_install.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_touchforms.yml $PLAYBOOK_TAGS
+commcare-cloud $ENV ansible-playbook deploy_http_proxy.yml $PLAYBOOK_TAGS
 
 commcare-cloud $ENV fab deploy:confirm=no,skip_record=yes --show=debug --set ignore_kafka_checkpoint_warning=true --branch=$BRANCH
 
