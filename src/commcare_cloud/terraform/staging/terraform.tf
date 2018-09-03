27e1100b7041d3076efe075e1b4ec3c0726983b0
Daniel Roberts <droberts@dimagi.com>
2018-09-03 16:51:24 -0500

    Delete terraform code we're not going to use

--- src/commcare_cloud/terraform/staging/terraform.tf
+++ src/commcare_cloud/terraform/staging/terraform.tf
@@ -89,28 +89,4 @@ module "commcarehq" {
       subnet_index          = 0
     }
   ]
-
-
-  # If using RDS, uncomment and update as necessary
-  # #######
-  # # RDS #
-  # #######
-  # rds_storage                    = "330"                                      # Setup the initial storage amount
-  # rds_engine                     = "postgresql"                                 # Specify the DB type; mysql, postgresql, aurora-mysql, etc
-  # rds_instance_type              = "db.t2.micro"                                   # Select teh size of your RDS instance
-  # rds_username                   = "root"                                       # Set the master username. rds_password should be passed via a secure method
-  # rds_storage_type               = "gp2"
-  # rds_backup_window              = "10:30-11:15"
-  # rds_backup_retention           = 7                                           # Keeps 30 days of snapshots
-  # rds_maintenance_window         = "sat:09:56-sun:10:26"
-  # rds_auto_minor_version_upgrade = false                                        # Whether or not to automatically update minor versions of the DB code, may cause service interruption.
-  # rds_multi_az                   = false                                        # Only set true on prod (or by request)
-  # rds_storage_encrypted          = true
-  # rds_port                       = "5432"                                       # DB listener port; 5432 = postgresql, 3306 = mysql
-  # rds_sg_ip_ingress              = ["${module.network.vpc-all-hosts-sg}"]
-  # rds_database_name              = "commcarehq"
-  # rds_prevent_destroy            = false
-  # rds_instance_count             = 1                                            # How many RDS instances to add to the cluster
-  # rds_engine_version             = "9.6.8-R1"                                   # Specific version to build
-
 }
