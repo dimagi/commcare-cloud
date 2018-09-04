f3c07965263e3a55fa38abab8ad7c5626ab95dc1
Sravan Reddy <sravfeyn@gmail.com>
2018-09-04 13:25:08 +0530

    typo

--- src/commcare_cloud/commands/migrations/copy_files.py
+++ src/commcare_cloud/commands/migrations/copy_files.py
@@ -271,7 +271,7 @@ def _run_auth_playbook(plan, environment, ansible_context, action, working_direc
         elif action == 'remove':
             _set_auth_key(plan.source_env, source_host, source_user, ansible_context, working_directory, True)
 
-    os.remove(os.path.join(working_directory, 'id_rsa.tmp')
+    os.remove(os.path.join(working_directory, 'id_rsa.tmp'))
 
 
 def _genearate_and_fetch_key(env, host, user, ansible_context, working_directory):
