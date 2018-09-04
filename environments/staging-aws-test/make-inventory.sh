22ddd060140ed59c4a677182878bfb966d016b8e
Daniel Roberts <droberts@dimagi.com>
2018-09-04 13:54:02 -0500

    Add public ip to staging-aws-test

--- environments/staging-aws-test/make-inventory.sh
+++ environments/staging-aws-test/make-inventory.sh
@@ -10,10 +10,10 @@ cat <(
     ) <(
     AWS_PROFILE=dimagi aws ec2 describe-instances \
         --filter \
-            "Name=tag-key,Values=Name" "Name=tag-value,Values=g2-bastion" \
+            "Name=tag-key,Values=Name" "Name=tag-value,Values=proxy1-staging" \
             "Name=instance-state-name,Values=running" \
-        --query "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].PrivateIpAddresses[0].PrivateIpAddress]" \
-        --output text --region us-east-1
+        --query "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].Association.PublicIp]" \
+        --output text --region us-east-1 | sed '$s/$/.public_ip/' \
     ) | xargs -n2 echo \
     | awk 'BEGIN { printf "sed " } { printf "-e '\''s/{{ "$2" }}/"$1"/'\'' " } END { printf "environments/staging-aws-test/inventory.ini.j2" }' \
     | bash > environments/staging-aws-test/inventory.ini
