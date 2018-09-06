ba7d44871a7799748ac07c12b9e774f190c934c7
Daniel Roberts <droberts@dimagi.com>
2018-09-06 14:36:21 -0700

    Switch to readable subnet refs in server list spec

--- src/commcare_cloud/terraform/modules/servers/main.tf
+++ src/commcare_cloud/terraform/modules/servers/main.tf
@@ -2,7 +2,7 @@ resource aws_instance "server" {
   count                   = "${length(var.servers)}"
   ami                     = "${var.server_image}"
   instance_type           = "${lookup(var.servers[count.index], "server_instance_type")}"
-  subnet_id               = "${var.subnet_options[lookup(var.servers[count.index], "subnet_index")]}"
+  subnet_id               = "${var.subnet_options[lookup(var.servers[count.index], "subnet")]}"
   key_name                = "g2-access"
   vpc_security_group_ids  = ["${var.security_groups}"]
   source_dest_check       = false
