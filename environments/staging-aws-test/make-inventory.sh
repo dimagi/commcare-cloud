ee6b77ec0aa294bea6279316ee13c0c6c0e19248
Daniel Roberts <droberts@dimagi.com>
2018-09-07 17:18:53 -0700

    Update staging-aws-test inventory with renamed redis node

--- environments/staging-aws-test/make-inventory.sh
+++ environments/staging-aws-test/make-inventory.sh
@@ -4,7 +4,7 @@ cat <(
         --filter \
             "Name=tag-key,Values=Name" "Name=tag-value,Values=*$name_tag*" \
             "Name=instance-state-name,Values=running" \
-            "Name=tag-key,Values=Environment" "Name=tag-value,Values=staging" \
+            "Name=tag-key,Values=Environment" "Name=tag-value,Values=staging-test" \
         --query "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].PrivateIpAddresses[0].PrivateIpAddress]" \
         --output text --region us-east-1
     ) <(
@@ -14,6 +14,10 @@ cat <(
             "Name=instance-state-name,Values=running" \
         --query "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].Association.PublicIp]" \
         --output text --region us-east-1 | sed '$s/$/.public_ip/' \
+    ) <(
+    AWS_PROFILE=dimagi aws elasticache describe-cache-clusters --show-cache-node-info \
+        --query 'CacheClusters[0].CacheNodes[0].Endpoint.Address' \
+        --output text --region us-east-1; echo staging-redis
     ) | xargs -n2 echo \
     | awk 'BEGIN { printf "sed " } { printf "-e '\''s/{{ "$2" }}/"$1"/'\'' " } END { printf "environments/staging-aws-test/inventory.ini.j2" }' \
     | bash > environments/staging-aws-test/inventory.ini
