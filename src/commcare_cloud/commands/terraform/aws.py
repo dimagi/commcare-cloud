d6dc9aec88c521c07e2585a1f890957726a22481
Daniel Roberts <droberts@dimagi.com>
2018-09-14 18:09:57 -0400

    Extend aws-list command to include public ip addresses
    for those VMs that have them.

--- src/commcare_cloud/commands/terraform/aws.py
+++ src/commcare_cloud/commands/terraform/aws.py
@@ -11,6 +11,23 @@ from commcare_cloud.commands.command_base import CommandBase
 from commcare_cloud.environment.main import get_environment
 
 
+def check_output(cmd_parts, env):
+
+    env_vars = os.environ.copy()
+    env_vars.update(env)
+    cmd = ' '.join(shlex_quote(arg) for arg in cmd_parts)
+    print_command('{} {}'.format(
+        ' '.join('{}={}'.format(key, value) for key, value in env.items()),
+        cmd,
+    ))
+    return subprocess.check_output(cmd_parts, env=env_vars)
+
+
+def aws_cli(environment, cmd_parts):
+    return json.loads(
+        check_output(cmd_parts, env={'AWS_PROFILE': environment.terraform_config.aws_profile}))
+
+
 class AwsList(CommandBase):
     command = 'aws-list'
     help = "List endpoints (ec2, rds, etc.) on AWS"
@@ -18,7 +35,9 @@ class AwsList(CommandBase):
     def run(self, args, unknown_args):
         environment = get_environment(args.env_name)
         config = environment.terraform_config
-        cmd_parts = [
+
+        # Private IP addresses
+        private_ip_query = aws_cli(environment, [
             'aws', 'ec2', 'describe-instances',
             '--filter', "Name=tag-key,Values=Name", "Name=tag-value,Values=*",
             "Name=instance-state-name,Values=running",
@@ -27,18 +46,27 @@ class AwsList(CommandBase):
             "--query", "Reservations[*].Instances[*][Tags[?Key=='Name'].Value, NetworkInterfaces[0].PrivateIpAddresses[0].PrivateIpAddress]",
             "--output", "json",
             "--region", config.region,
-        ]
+        ])
+        name_private_ip_pairs = [(item[0][0][0], item[0][1]) for item in private_ip_query]
 
-        env_vars = {'AWS_PROFILE': environment.terraform_config.aws_profile}
-        all_env_vars = os.environ.copy()
-        all_env_vars.update(env_vars)
-        cmd = ' '.join(shlex_quote(arg) for arg in cmd_parts)
-        print_command('{} {}'.format(
-            ' '.join('{}={}'.format(key, value) for key, value in env_vars.items()),
-            cmd,
-        ))
-        raw_list = json.loads(subprocess.check_output(cmd_parts, env=all_env_vars, stdin=subprocess.PIPE))
-        nice_list = [(item[0][0][0], item[0][1]) for item in raw_list]
-        for name, ip in nice_list:
+        # Public IP addresses
+        public_ip_query = aws_cli(environment, [
+            'aws', 'ec2', 'describe-instances',
+            '--filter', "Name=tag-key,Values=Name", "Name=tag-value,Values=*",
+            "Name=instance-state-name,Values=running",
+            "Name=tag-key,Values=Environment",
+            "Name=tag-value,Values={}".format(config.environment),
+            "--query",
+            "Reservations[*].Instances[*][Tags[?Key=='Name'].Value[],NetworkInterfaces[0].Association.PublicIp]",
+            "--output", "json",
+            "--region", config.region,
+        ])
+        name_public_ip_pairs = [(item[0][0][0], item[0][1]) for item in public_ip_query
+                                if item[0][1] is not None]
+
+        for name, ip in name_private_ip_pairs:
             print('{}\t{}'.format(ip, name))
+
+        for name, ip in name_public_ip_pairs:
+            print('{}\t{}.public_ip'.format(ip, name))
         return 0
