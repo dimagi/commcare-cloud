4b84c69ea9dfc1253e75070fca27213385a2591b
Daniel Roberts <droberts@dimagi.com>
2018-10-04 19:38:35 -0300

    Add aws-fill-inventory command to replace make-inventory.sh script

--- environments/staging-aws-test/make-inventory.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-ENV=$1
-cchq ${ENV} aws-list | xargs -n2 echo \
-    | awk 'BEGIN { printf "sed " } { printf "-e '\''s/{{ "$2" }}/"$1"/'\'' " } END { printf "environments/'${ENV}'/inventory.ini.j2" }' \
-    | bash > environments/${ENV}/inventory.ini
