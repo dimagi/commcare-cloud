2fe739e85cc529e6f330c8e0f97b8e14ac2b9011
Daniel Roberts <droberts@dimagi.com>
2018-10-04 19:23:53 -0300

    Generalize make-inventory.sh to also generate staging inventory

--- environments/staging-aws-test/make-inventory.sh
+++ environments/staging-aws-test/make-inventory.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-cchq staging-aws-test aws-list | xargs -n2 echo \
-    | awk 'BEGIN { printf "sed " } { printf "-e '\''s/{{ "$2" }}/"$1"/'\'' " } END { printf "environments/staging-aws-test/inventory.ini.j2" }' \
-    | bash > environments/staging-aws-test/inventory.ini
+ENV=$1
+cchq ${ENV} aws-list | xargs -n2 echo \
+    | awk 'BEGIN { printf "sed " } { printf "-e '\''s/{{ "$2" }}/"$1"/'\'' " } END { printf "environments/'${ENV}'/inventory.ini.j2" }' \
+    | bash > environments/${ENV}/inventory.ini
