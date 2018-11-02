bf5cbc4874c86040c5c891df5948401df096b15c
Daniel Roberts <droberts@dimagi.com>
2018-11-02 12:10:32 -0500

    Move site_locations to proxy.yml and add limit_req_zones

--- src/commcare_cloud/environment/schemas/proxy.py
+++ src/commcare_cloud/environment/schemas/proxy.py
@@ -19,6 +19,9 @@ class ProxyConfig(jsonobject.JsonObject):
 
     special_sites = jsonobject.ListProperty(str)
 
+    limit_req_zones = jsonobject.ListProperty(lambda: LimitReqZone)
+    site_locations = jsonobject.ListProperty(lambda: SiteLocation)
+
     COMMTRACK_SITE_HOST = jsonobject.StringProperty(exclude_if_none=True)
     commtrack_nginx_combined_cert_value = jsonobject.StringProperty(exclude_if_none=True)
     commtrack_key_value = jsonobject.StringProperty(exclude_if_none=True)
@@ -48,3 +51,32 @@ class ProxyConfig(jsonobject.JsonObject):
     @classmethod
     def get_claimed_variables(cls):
         return set(cls._properties_by_key.keys())
+
+
+class LimitReqZone(jsonobject.JsonObject):
+    _allow_dynamic_properties = False
+    name = jsonobject.StringProperty()
+    zone = jsonobject.StringProperty(choices=["$digest_submission"])
+    size = jsonobject.StringProperty()
+    rate = jsonobject.StringProperty()
+
+
+class SiteLocation(jsonobject.JsonObject):
+    _allow_dynamic_properties = False
+
+    name = jsonobject.StringProperty(required=True)
+    balancer = jsonobject.StringProperty(exclude_if_none=True)
+    proxy_next_upstream_tries = jsonobject.IntegerProperty(exclude_if_none=True)
+    proxy_buffers = jsonobject.StringProperty(exclude_if_none=True)
+    proxy_buffer_size = jsonobject.StringProperty(exclude_if_none=True)
+    client_body_buffer_size = jsonobject.StringProperty(exclude_if_none=True)
+    limit_reqs = jsonobject.ListProperty(lambda: LimitReq, exclude_if_none=True)
+    try_files = jsonobject.StringProperty(exclude_if_none=True)
+    auth_basic = jsonobject.StringProperty(exclude_if_none=True)
+    auth_basic_user_file = jsonobject.StringProperty(exclude_if_none=True)
+
+
+class LimitReq(jsonobject.JsonObject):
+    zone_name = jsonobject.StringProperty()
+    burst = jsonobject.IntegerProperty()
+    nodelay = jsonobject.BooleanProperty()
