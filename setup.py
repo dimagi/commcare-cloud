c151c68e65b895add95f68868353a2b4c8c67d85
Daniel Roberts <droberts@dimagi.com>
2018-11-06 17:28:41 -0500

    Move boto3 to default installs

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ test_deps = [
 ]
 aws_deps = [
     'awscli',
-    'boto3',
 ]
 extras = {
     'test': test_deps,
@@ -37,6 +36,7 @@ setup(
         'ansible==2.5.0',
         'argparse>=1.4',
         'attrs>=18.1.0',
+        'boto3',
         'clint',
         'couchdb-cluster-admin>=0.4.1',
         'cryptography>=2.3',  # security update
