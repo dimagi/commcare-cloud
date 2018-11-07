695414ba7619242e1dfda877848fc9587ba67ace
Daniel Roberts <droberts@dimagi.com>
2018-11-07 15:57:19 -0500

    Template terraform servers so each is addressed by name

--- src/commcare_cloud/terraform/modules/servers/outputs.tf
+++ src/commcare_cloud/terraform/modules/servers/outputs.tf
@@ -1,11 +1,7 @@
-output "count" {
-  value = "${aws_instance.server.count}"
-}
-
 output "server" {
-  value = "${aws_instance.server.*.id}"
+  value = "${aws_instance.server.id}"
 }
 
 output "server_private_ip" {
-  value = "${aws_instance.server.*.private_ip}"
+  value = "${aws_instance.server.private_ip}"
 }
