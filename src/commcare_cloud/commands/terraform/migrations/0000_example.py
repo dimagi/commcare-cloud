cb941d1c64fe84ca11e69ebb2eb73b6a4740b6bf
Daniel Roberts <droberts@dimagi.com>
2018-11-08 11:12:43 -0500

    Better document the meaning of falsy return value for get_new_resource_address

--- src/commcare_cloud/commands/terraform/migrations/0000_example.py
+++ src/commcare_cloud/commands/terraform/migrations/0000_example.py
@@ -2,10 +2,12 @@ def get_new_resource_address(environment, old_resource_address):
     """
     Specify migration by specifying for a given old address what the new address should be
 
+    A falsy value is interpreted as skip / no change.
     All addresses will be / should be given in the format used in `terraform state list`.
 
     The migration is then essentially (in peudocode, and with better edge-case management):
 
     for resource in `terraform state list`:
-        `terraform state mv ${resource} ${get_new_resource_address(environment, resource)}`
+        if get_new_resource_address(environment, resource):
+            `terraform state mv ${resource} ${get_new_resource_address(environment, resource)}`
     """
