549802719c14fd9b969c6c825e0e19abf4be5293
Daniel Roberts <droberts@dimagi.com>
2018-11-09 17:58:09 -0500

    Add courtesy message to inventory templating–related failure

--- tests/test_autogen_environments.sh
+++ tests/test_autogen_environments.sh
@@ -15,5 +15,10 @@ if git diff-index --quiet HEAD --; then
     exit 0
 else
     # Changes
+    echo "Hey, did you edit inventory.ini when you should have edited inventory.ini.j2?"
+    echo "That's a common cause of this test failure."
+    echo "If so, make your change to inventory.ini.j2 and run"
+    echo "  cchq <env> aws-fill-inventory --cached"
+    echo "to fix this test"
     exit 1
 fi
