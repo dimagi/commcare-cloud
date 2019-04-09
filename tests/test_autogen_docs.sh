042f3ff69f7de58653dce7e5873d5e94a6caed28
Daniel Roberts <droberts@dimagi.com>
2019-04-09 15:54:09 -0400

    Add better error message to test_autogen_docs.sh

--- tests/test_autogen_docs.sh
+++ tests/test_autogen_docs.sh
@@ -4,4 +4,9 @@ set -e
 make clean
 make
 
-./tests/fail_if_there_is_a_git_diff.sh
+./tests/fail_if_there_is_a_git_diff.sh || echo "
+If you're getting a failure here with a change to the changelog docs
+make sure you're following the directions in
+https://github.com/dimagi/commcare-cloud/blob/master/changelog/README.md
+and not editing changelog markdown files directly.
+"
