6415a254da91d8b7d3ca954a638b7d9b4322cb59
Daniel Roberts <droberts@dimagi.com>
2019-04-29 12:35:51 -0500

    Explicitly use python2 for commcare-cloud env

--- control/init.sh
+++ control/init.sh
@@ -50,7 +50,13 @@ if [ -z ${TRAVIS_TEST} ]; then
     source virtualenvwrapper.sh
     if [ ! -d ~/.virtualenvs/ansible ]; then
         echo "Creating ansible virtualenv..."
-        mkvirtualenv ansible
+        mkvirtualenv ansible --python $(which python2)
+    # If we by mistake are using a py3 env (unsupported), then replace with a py2 env
+    elif ~/.virtualenvs/ansible/bin/python -c 'print ""' 2> /dev/null; [ "$?" -ne "0" ]; then
+        echo "Replacing ansible py3 virtualenv with py2 virtualenv..."
+        deactivate 2> /dev/null || :  # deactivate if in a virtualenv, else ignore error
+        rmvirtualenv ansible
+        mkvirtualenv ansible --python $(which python2)
     else
         workon ansible
     fi
