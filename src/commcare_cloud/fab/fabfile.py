848389c2a1bed05dc6d4bf3508e23661f64f141a
Daniel Roberts <droberts@dimagi.com>
2019-08-22 16:24:53 -0500

    Fix serious formplayer issue after deploy with stop-gap measure
    My temporary fix is to just restart formplayer after deploy,
    even though it wasnt actually updated. And since there's a restart there
    will still be the 1-2m of formplayer downtime that we're "used" to. The
    longer term fix will be to move the formplayer directory out from inside
    the commcare-hq code directory so that deploying commcare doesn't change
    the file pointed to by the formplayer jar path that supervisor uses to run the jar.

--- src/commcare_cloud/fab/fabfile.py
+++ src/commcare_cloud/fab/fabfile.py
@@ -799,6 +799,7 @@ def silent_services_restart(use_current_release=False):
     """
     execute(db.set_in_progress_flag, use_current_release)
     if not env.is_monolith:
+        execute(supervisor.restart_formplayer)
         execute(supervisor.restart_all_except_webworkers)
     execute(supervisor.restart_webworkers)
 
