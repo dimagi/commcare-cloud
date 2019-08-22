34ccec1c899d67b36f985e8f794bb31f942ec531
Daniel Roberts <droberts@dimagi.com>
2019-08-22 11:14:18 -0500

    Add --skip-record and --quiet args to `deploy` command

--- src/commcare_cloud/commands/deploy.py
+++ src/commcare_cloud/commands/deploy.py
@@ -15,15 +15,32 @@ class Deploy(CommandBase):
         Argument('--resume', action='store_true', help="""
             Rather than starting a new deploy, start where you left off the last one.
         """),
+        Argument('--skip-record', action='store_true', help="""
+            Skip the steps involved in recording and announcing the fact of the deploy.
+        """),
+        shared_args.QUIET_ARG,
         shared_args.BRANCH_ARG,
     )
 
     def run(self, args, unknown_args):
         check_branch(args)
         environment = get_environment(args.env_name)
-        if args.resume:
-            fab_func_args = ':resume=yes'
-        else:
-            fab_func_args = ''
+        fab_func_args = self.get_fab_func_args(args)
         commcare_cloud(environment.name, 'fab', 'deploy_commcare{}'.format(fab_func_args),
                        branch=args.branch, *unknown_args)
+
+    @staticmethod
+    def get_fab_func_args(args):
+        fab_func_args = []
+
+        if args.quiet:
+            fab_func_args.append('confirm=no')
+        if args.resume:
+            fab_func_args.append('resume=yes')
+        if args.skip_record:
+            fab_func_args.append('skip_record=yes')
+
+        if fab_func_args:
+            return ':{}'.format(','.join(fab_func_args))
+        else:
+            return ''
