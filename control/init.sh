a31508990f1f28b928d2bdfde28ac17c90db9d30
Daniel Roberts <droberts@dimagi.com>
2019-09-26 22:26:26 -0500

    Revert "Debug travis timeout by removing concurrency from control/init.sh"
    This reverts commit 1352434bb1b65d73a84a85af90aa2435322c8568.

--- control/init.sh
+++ control/init.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-set -x
 
 if [[ $_ == $0 ]]
 then
@@ -93,13 +92,14 @@ if [ -z "$(which manage-commcare-cloud)" ]; then
     # installs strictly what's in requirements.txt, so versions are pre-pinned
     cd ${COMMCARE_CLOUD_REPO} && pip install pip-tools && pip-sync && pip install -e . && cd -
 else
-    { cd ${COMMCARE_CLOUD_REPO} && pip install pip-tools && pip-sync && pip install -e . && cd - ; }
+    { cd ${COMMCARE_CLOUD_REPO} && pip install pip-tools && pip-sync && pip install -e . && cd - ; } &
 fi
 
 echo "Downloading dependencies from galaxy and pip"
 export ANSIBLE_ROLES_PATH=~/.ansible/roles
-pip install pip --upgrade
-manage-commcare-cloud install  # includes ansible-galaxy install
+pip install pip --upgrade &
+manage-commcare-cloud install & # includes ansible-galaxy install
+wait
 
 # workaround for some envs that got in a bad state
 python -c 'import Crypto' || {
