76c633865e7c137bc1574d77eb4924f19b9ca28e
Daniel Roberts <droberts@dimagi.com>
2019-11-04 11:02:27 -0600

    Add bare_non_cchq_environment meta property for confluence envs (and the like)

--- src/commcare_cloud/environment/schemas/meta.py
+++ src/commcare_cloud/environment/schemas/meta.py
@@ -7,3 +7,4 @@ class MetaConfig(jsonobject.JsonObject):
     env_monitoring_id = jsonobject.StringProperty(required=True)
     users = jsonobject.ListProperty(unicode, required=True)
     slack_alerts_channel = jsonobject.StringProperty()
+    bare_non_cchq_environment = jsonobject.BooleanProperty(default=False)
