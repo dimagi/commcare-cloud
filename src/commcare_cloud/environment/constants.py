9daed76b64528f6bdbcbdd8cd13278e46adb76e5
Simon Kelly <skelly@dimagi.com>
2019-12-06 13:49:43 +0200

    setup plproxy 'standby' database

--- src/commcare_cloud/environment/constants.py
+++ src/commcare_cloud/environment/constants.py
@@ -7,6 +7,7 @@ class _Constants(jsonobject.JsonObject):
     ucr_db_name = jsonobject.StringProperty(default='commcarehq_ucr')
     synclogs_db_name = jsonobject.StringProperty(default='commcarehq_synclogs')
     form_processing_proxy_db_name = jsonobject.StringProperty(default='commcarehq_proxy')
+    form_processing_proxy_standby_db_name = jsonobject.StringProperty(default='commcarehq_proxy_standby')
 
 
 constants = _Constants()
