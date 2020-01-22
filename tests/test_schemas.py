fa108a8e326e34a09629d18cc0c47cf6479f9eb4
Simon Kelly <skelly@dimagi.com>
2020-01-22 14:46:17 +0200

    make get_machine_alias more strict about only returning a group that has a single host (not child groups)

---
+++ tests/test_schemas.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import itertools
+import os
+
+from mock import patch
+from nose.tools import assert_equal
+
+from commcare_cloud.environment.main import get_environment
+from commcare_cloud.environment.schemas.app_processes import get_machine_alias
+
+TEST_ENVIRONMENTS_DIR = os.path.join(os.path.dirname(__file__), 'test_envs')
+
+
+@patch('commcare_cloud.environment.paths.ENVIRONMENTS_DIR', TEST_ENVIRONMENTS_DIR)
+def test_get_machine_alias():
+    env = get_environment('small_cluster')
+
+    all_hosts = set(itertools.chain.from_iterable(env.groups.values()))
+    assert_equal(all_hosts, {'172.19.3.0', '172.19.3.1', '172.19.3.2', '172.19.3.3'})
+    aliases = set([get_machine_alias(env, host) for host in all_hosts])
+    assert_equal(aliases, {'demo_server0', 'demo_server1', 'demo_server2', 'demo_server3'})
