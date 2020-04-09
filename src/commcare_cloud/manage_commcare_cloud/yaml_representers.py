ffe806d8a6060e3bad1f1f0f4e6ffd02ab407201
Amit Phulera <amitphulera999@gmail.com>
2020-04-09 13:29:30 +0530

    fix for represent_unicode error message

--- src/commcare_cloud/manage_commcare_cloud/yaml_representers.py
+++ src/commcare_cloud/manage_commcare_cloud/yaml_representers.py
@@ -17,6 +17,10 @@ def change_style(style, representer):
         return scalar
     return new_representer
 
+if six.PY3:
+    represent_unicode = yaml.representer.SafeRepresenter.represent_str
+else:
+    represent_unicode = yaml.representer.SafeRepresenter.represent_unicode
 
-represent_literal_unicode = change_style('|', SafeRepresenter.represent_unicode)
+represent_literal_unicode = change_style('|', represent_unicode)
 yaml.add_representer(LiteralUnicode, represent_literal_unicode, Dumper=yaml.SafeDumper)
