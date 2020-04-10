7868e5ecf7d423e1e8a40aca2f0b9235f70049f3
Schauhan <schauhan@dimagi.com>
2020-04-10 15:11:54 +0000

    added intruder.sh script

---
+++ src/commcare_cloud/ansible/roles/devops_scripts/templates/intruder.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#Filename: intruder_detect.sh
+#Description: Intruder reporting tool with secure log as input
+AUTHLOG=/var/log/auth.log
+AUTHLOG1=/var/log/auth.log.1
+if [[ -n $1 ]];
+then
+  AUTHLOG=$1
+  echo Using Log file : $AUTHLOG
+fi
+
+# Collect the failed login attempts
+FAILED_LOG=/tmp/failed.$$.log
+egrep "Failed pass" $AUTHLOG > $FAILED_LOG 
+egrep "Failed pass" $AUTHLOG1 >> $FAILED_LOG 
+
+# Collect the successful login attempts
+SUCCESS_LOG=/tmp/success.$$.log
+egrep "Accepted password|Accepted publickey|keyboard-interactive" $AUTHLOG > $SUCCESS_LOG
+egrep "Accepted password|Accepted publickey|keyboard-interactive" $AUTHLOG1 > $SUCCESS_LOG
+
+# extract the users who failed
+failed_users=$(cat $FAILED_LOG | grep -oP '(?<=for )[^ ]*' | sort | uniq)
+
+# extract the users who successfully logged in
+success_users=$(cat $SUCCESS_LOG | grep -oP '(?<=for )[^ ]*' | sort | uniq)
+# extract the IP Addresses of successful and failed login attempts
+failed_ip_list="$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $FAILED_LOG | sort | uniq)"
+success_ip_list="$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $SUCCESS_LOG | sort | uniq)"
+
+# Print the heading
+printf "%-10s|%-10s|%-10s|%-15s|%-15s|%s\n" "Status" "User" "Attempts" "IP address" "Host" "Time range"
+
+# Loop through IPs and Users who failed.
+
+for ip in $failed_ip_list;
+do
+  for user in $failed_users;
+    do
+    # Count failed login attempts by this user from this IP
+    attempts=`grep $ip $FAILED_LOG | grep " $user " | wc -l`
+
+    if [ $attempts -ne 0 ]
+    then
+      first_time=`grep $ip $FAILED_LOG | grep " $user " | head -1 | cut -c-16`
+      time="$first_time"
+      if [ $attempts -gt 1 ]
+      then
+        last_time=`grep $ip $FAILED_LOG | grep " $user " | tail -1 | cut -c-16`
+        time="$first_time -> $last_time"
+      fi
+      HOST=$(host $ip 8.8.8.8 | tail -1 | awk '{ print $NF }' )
+      printf "%-10s|%-10s|%-10s|%-15s|%-15s|%-s\n" "Failed" "$user" "$attempts" "$ip"  "$HOST" "$time";
+    fi
+  done
+done
+
+for ip in $success_ip_list;
+do
+  for user in $success_users;
+    do
+    # Count successful login attempts by this user from this IP
+    attempts=`grep $ip $SUCCESS_LOG | grep " $user " | wc -l`
+
+    if [ $attempts -ne 0 ]
+    then
+      first_time=`grep $ip $SUCCESS_LOG | grep " $user " | head -1 | cut -c-16`
+      time="$first_time"
+      if [ $attempts -gt 1 ]
+      then
+        last_time=`grep $ip $SUCCESS_LOG | grep " $user " | tail -1 | cut -c-16`
+        time="$first_time -> $last_time"
+      fi
+      HOST=$(host $ip 8.8.8.8 | tail -1 | awk '{ print $NF }' )
+      printf "%-10s|%-10s|%-10s|%-15s|%-15s|%-s\n" "Success" "$user" "$attempts" "$ip"  "$HOST" "$time";
+    fi
+  done
+done
+
+rm -f $FAILED_LOG
+rm -f $SUCCESS_LOG
