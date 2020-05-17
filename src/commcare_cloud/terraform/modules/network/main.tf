9adf257f48ae46914169e41dca39180832e0cb36
Daniel Roberts <droberts@dimagi.com>
2020-05-16 19:56:27 -0500

    Revert cutting off proxy from public internet 80/443
    but leave a todo comment to have us remove that once we're fully on GA + ALB + WAF

--- src/commcare_cloud/terraform/modules/network/main.tf
+++ src/commcare_cloud/terraform/modules/network/main.tf
@@ -201,6 +201,24 @@ resource "aws_security_group" "proxy-sg" {
   name   = "proxy-sg-${var.env}"
   vpc_id = "${aws_vpc.main.id}"
 
+  /* todo: delete this block to cut off direct public internet access to proxy { */
+  ingress {
+    from_port =         "80"
+    to_port =           "80"
+    protocol =          "tcp"
+    cidr_blocks =       ["0.0.0.0/0"]
+    ipv6_cidr_blocks =  ["::/0"]
+  }
+
+  ingress {
+    protocol =          "tcp"
+    to_port =           "443"
+    from_port =         "443"
+    cidr_blocks =       ["0.0.0.0/0"]
+    ipv6_cidr_blocks =  ["::/0"]
+  }
+  /* } */
+
   ingress {
     from_port   = 0
     to_port     = 0
