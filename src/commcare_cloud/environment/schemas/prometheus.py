8460430cc3742ec23ba153d2464e7cebbb34ed33
rohit-dimagi <52556758+rohit-dimagi@users.noreply.github.com>
2020-05-18 22:56:44 +0530

    Prometheus setup on icds-staging env (#3823)

--- src/commcare_cloud/environment/schemas/prometheus.py
+++ src/commcare_cloud/environment/schemas/prometheus.py
@@ -3,7 +3,7 @@ import jsonobject
 
 class PrometheusConfig(jsonobject.JsonObject):
     prometheus_monitoring_enabled = jsonobject.BooleanProperty(required=True)
-    grafana_security = jsonobject.DictProperty(required=True)
+    grafana_security = jsonobject.DictProperty()
 
     def to_generated_variables(self):
         variables = self.to_json()
