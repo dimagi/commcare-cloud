302a2f721e7cd17dddb5dc80a9c0c96f99683d95
Shyamkumar L Chauhan <schauhan@dimagi.com>
2020-06-23 18:06:26 +0530

    scripts to automate to generate the login and sudo records

---
+++ src/commcare_cloud/ansible/roles/devops_scripts/templates/audit_log.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+### Set initial time of file
+if [ ! -f /var/log/login_audit ];
+   then
+LTIME=`stat -c %Y /var/log/auth.log.1`
+fi
+sleep 60
+ATIME=`stat -c %Y /var/log/auth.log.1`
+if [ "$ATIME" != "$LTIME" ];
+   then    
+   /usr/local/sbin/intruder.sh >> /var/log/login_audit.log
+   /usr/local/sbin/sudo-report.sh >> /var/log/sudo_audit.log
+   LTIME=$ATIME
+fi
