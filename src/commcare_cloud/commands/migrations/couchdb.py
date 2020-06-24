e791b03402a594b970aff7194e80083c1deaa0c4
Simon Kelly <skelly@dimagi.com>
2020-06-24 13:16:28 +0200

    don't require running plan before describe

--- src/commcare_cloud/commands/migrations/couchdb.py
+++ src/commcare_cloud/commands/migrations/couchdb.py
@@ -311,12 +311,16 @@ def describe(migration):
     print_db_info(migration.target_couch_config)
 
     puts(u'\nShard allocation')
-    diff_with_db = diff_plan(migration)
-    if diff_with_db:
-        puts(color_highlight('DB allocation differs from plan:\n'))
-        puts("{}\n\n".format(diff_with_db))
-    else:
-        puts(color_success('DB allocation matches plan.'))
+    diff_with_db = None
+    if os.path.exists(migration.shard_plan_path):
+        diff_with_db = diff_plan(migration)
+        if diff_with_db:
+            puts(color_highlight(u'DB allocation differs from plan:\n'))
+            puts(u"{}\n\n".format(diff_with_db))
+        else:
+            puts(color_success(u'DB allocation matches plan.'))
+
+    if not diff_with_db:
         print_shard_table([
             get_shard_allocation(migration.target_couch_config, db_name)
             for db_name in sorted(get_db_list(migration.target_couch_config.get_control_node()))
