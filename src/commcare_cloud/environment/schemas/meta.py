b7b6b2d5057b08e4d93ce9d3d74628d75a37f654
Simon Kelly <skelly@dimagi.com>
2020-06-30 16:31:19 +0200

    clone additional repos

--- src/commcare_cloud/environment/schemas/meta.py
+++ src/commcare_cloud/environment/schemas/meta.py
@@ -1,6 +1,13 @@
 import jsonobject
 
 
+class GitRepository(jsonobject.JsonObject):
+    url = jsonobject.StringProperty(required=True)
+    dest = jsonobject.StringProperty(default="")  # relative to the release directory
+    version = jsonobject.StringProperty(default="master")
+    requirements_path = jsonobject.StringProperty(default="requirements.txt")
+
+
 class MetaConfig(jsonobject.JsonObject):
     _allow_dynamic_properties = False
     deploy_env = jsonobject.StringProperty(required=True)
@@ -9,3 +16,4 @@ class MetaConfig(jsonobject.JsonObject):
     users = jsonobject.ListProperty(unicode, required=True)
     slack_alerts_channel = jsonobject.StringProperty()
     bare_non_cchq_environment = jsonobject.BooleanProperty(default=False)
+    git_repositories = jsonobject.ListProperty(GitRepository)
