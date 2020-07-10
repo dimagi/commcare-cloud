ebdbbd7f610d4df050d05d2b6efa6477eda7632a
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2020-07-10 18:50:34 +0530

    adding ports information in new_environment doc, showing csv file into markdown table. updated ports verification scripts to take named args

--- scripts/verify_ports_accessibility.sh
+++ scripts/verify_ports_accessibility.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
 ### run script by passing argument in quotes 
-## example: bash scriptname.sh "80 6379 3389 443"
+## example: bash verify_ports_accessibility.sh ip="IPaddres" ports="80 443 9300 6379"
 
-ports=$1
+for args in "$@"
+do
+
+    key=$(echo $args | cut -f1 -d=)
+    value=$(echo $args | cut -f2 -d=)   
+
+    case "$key" in
+            ip)       ip=${value} ;;
+            ports)    ports=${value} ;;     
+            *)   
+    esac    
+
+done
+
+address=${ip}
+query_ports=${ports}
 restricted_ports=()
 count=0
-for p in ${ports}
+for p in ${query_ports}
 do
-  cmd=`nc -z 0.0.0.0 $p`
-  if [ $p -eq 80 ] || [ $p -eq 443 ] && [ $? -eq 0 ]; then
+  cmd=`nc -z ${address} ${p}`
+  result=$?
+  if [ ${p} -eq 80 ] || [ ${p} -eq 443 ] && [ ${result} -eq 0 ]; then
     count=`expr $count + 1`
   else
-	  restricted_ports+=("${p}")
+    restricted_ports+=("${p}")
   fi
 done
-if [ $count -eq 2 ]; then
-	echo "All required ports are accessible"
+
+if [ ${count} -eq 2 ]; then
+  echo "All required ports are accessible"
 else
-	echo "All required ports are not accessible. Please check"
+  echo "All required ports are not accessible. Please check"
 fi
-echo -e "The following required ports aren't accessible: ${restricted_ports[*]} \n For more information visit ../docs/Commcare_Ports_information.xlsx"
+echo -e "The following required ports aren't accessible: ${restricted_ports[*]} \n For more information visit ../docs/Commcare_Ports_information.md"
