5b7e55018c297dbe4258a80a93355e17821bca1e
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2020-07-13 14:20:35 +0530

    updated verify_ports script with limited ports desc.

--- scripts/verify_ports_accessibility.sh
+++ scripts/verify_ports_accessibility.sh
@@ -3,6 +3,7 @@
 ### run script by passing argument in quotes 
 ## example: bash verify_ports_accessibility.sh ip="IPaddres" ports="80 443 9300 6379"
 
+# reading arguments
 for args in "$@"
 do
 
@@ -17,9 +18,29 @@ do
 
 done
 
+# retriving port information with static values
+function get_port_desc()
+{
+  port=$1
+  local AllPorts="SSH:22 https:443 http:80 Monolith_Commcare:9010 Formplayer:8181 Kafka:9092 Zookeeper:2181 Redis:6379 PostgreSQL:5432 PgBouncer:6432 RabbitMQ:5672 ElasticSearch:9200 ES_Cluster:9300 CouchDB:5984,4369 Celery_port:5555 Mail/SMTP:25,465,587"
+  desc=${port}
+  for pi in ${AllPorts} ;
+  do
+     if [[ "$pi" =~ .*"${port}".* ]]; then
+          desc="${pi}"
+     fi
+  done
+
+  #returns given #port if not found the port desc
+  echo ${desc}
+}
+
+# -- 
+
 address=${ip}
 query_ports=${ports}
 restricted_ports=()
+portdesc=()
 count=0
 for p in ${query_ports}
 do
@@ -28,7 +49,8 @@ do
   if [ ${p} -eq 80 ] || [ ${p} -eq 443 ] && [ ${result} -eq 0 ]; then
     count=`expr $count + 1`
   else
-    restricted_ports+=("${p}")
+    #restricted_ports+=("${p}")
+    restricted_ports+="$(get_port_desc ${p}) \n"
   fi
 done
 
@@ -37,4 +59,5 @@ if [ ${count} -eq 2 ]; then
 else
   echo "All required ports are not accessible. Please check"
 fi
-echo -e "The following required ports aren't accessible: ${restricted_ports[*]} \n For more information visit ../docs/Commcare_Ports_information.md"
+
+echo -e "The following required ports aren't accessible:\n${restricted_ports[*]} For more information visit ../docs/Commcare_Ports_information.md"
