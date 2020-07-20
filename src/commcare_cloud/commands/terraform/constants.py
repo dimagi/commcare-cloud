43458aa2e98d1575061942ca0a1ea0cbcc9c79f8
Daniel Roberts <droberts@dimagi.com>
2020-07-20 11:18:42 -0500

    Add bulk app translations to waf allow list

--- src/commcare_cloud/commands/terraform/constants.py
+++ src/commcare_cloud/commands/terraform/constants.py
@@ -10,6 +10,7 @@ COMMCAREHQ_XML_POST_URLS_REGEX = r"""
 ^/a/([\w\.:-]+)/apps/edit_form_attr/([\w-]+)/([\w-]+)/([\w-]+)/$
 ^/a/([\w\.:-]+)/apps/edit_form_attr_api/([\w-]+)/([\w-]+)/([\w-]+)/$
 ^/a/([\w\.:-]+)/apps/patch_xform/([\w-]+)/([\w-]+)/$
+^/a/([\w\.:-]+)/apps/view/([\w-]+)/languages/bulk_app_translations/upload/$
 ^/a/([\w\.:-]+)/cloudcare/api/readable_questions/$
 ^/a/([\w\.:-]+)/cloudcare/apps/report_formplayer_error
 ^/a/([\w\.:-]+)/data/export/custom/download_data_files/$
