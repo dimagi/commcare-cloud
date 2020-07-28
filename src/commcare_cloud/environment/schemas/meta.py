cf875ecd80e48dd2e95ac5d78f21bce54b51f3f6
Simon Kelly <skelly@dimagi.com>
2020-07-28 21:26:34 +0200

    Revert "Revert "ansible support for checking out additional git repositories""

--- src/commcare_cloud/environment/schemas/meta.py
+++ src/commcare_cloud/environment/schemas/meta.py
@@ -1,6 +1,22 @@
 import jsonobject
 
 
+class GitRepository(jsonobject.JsonObject):
+    url = jsonobject.StringProperty(required=True)
+    dest = jsonobject.StringProperty(required=True)  # relative to the code_source/external directory
+    version = jsonobject.StringProperty(default="master")
+    requirements_path = jsonobject.StringProperty(default="requirements.txt")
+
+    @property
+    def relative_dest(self):
+        return "extensions/" + self.dest
+
+    def to_generated_variables(self):
+        vars = self.to_json()
+        vars["dest"] = self.relative_dest
+        return vars
+
+
 class MetaConfig(jsonobject.JsonObject):
     _allow_dynamic_properties = False
     deploy_env = jsonobject.StringProperty(required=True)
@@ -9,3 +25,4 @@ class MetaConfig(jsonobject.JsonObject):
     users = jsonobject.ListProperty(unicode, required=True)
     slack_alerts_channel = jsonobject.StringProperty()
     bare_non_cchq_environment = jsonobject.BooleanProperty(default=False)
+    git_repositories = jsonobject.ListProperty(GitRepository)
