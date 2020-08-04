3236785e696a72cb636049a4c854779a7e99ad42
Ali <ali.rizvi1@gmail.com>
2020-08-04 09:45:54 -0700

    Add commit field to record_successful_deploy using deploy_ref

--- src/commcare_cloud/fab/operations/release.py
+++ src/commcare_cloud/fab/operations/release.py
@@ -373,12 +373,14 @@ def record_successful_deploy():
             '%(virtualenv_current)s/bin/python manage.py '
             'record_deploy_success --user "%(user)s" --environment '
             '"%(environment)s" --url %(url)s --minutes %(minutes)s --mail_admins'
+            '--commit %(commit)s'
         ) % {
             'virtualenv_current': env.py3_virtualenv_current,
             'user': env.user,
             'environment': env.deploy_env,
             'url': env.deploy_metadata.diff.url,
-            'minutes': str(int(delta.total_seconds() // 60))
+            'minutes': str(int(delta.total_seconds() // 60)),
+            'commit': env.deploy_metadata.deploy_ref
         })
 
 
