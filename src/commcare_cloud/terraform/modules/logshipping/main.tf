0e93385d65c4d26e9ed7556af8823cc7ef91e3ea
Kevin Nguyen <knguyen@dimagi.com>
2020-08-04 14:32:06 -0400

    logshipping firehose stream renaming

--- src/commcare_cloud/terraform/modules/logshipping/main.tf
+++ src/commcare_cloud/terraform/modules/logshipping/main.tf
@@ -1,7 +1,7 @@
 locals {
   log_bucket_name = "dimagi-commcare-${var.environment}-logs"
-  log_bucket_prefix = "frontend-logs-${var.environment}"
-  log_bucket_error_prefix = "frontend-logs-${var.environment}-error"
+  formplayer_request_response_log_bucket_prefix = "formplayer-request-response-logs-${var.environment}"
+  formplayer_request_response_log_bucket_error_prefix = "formplayer-request-response-logs-${var.environment}-error"
 }
 
 resource "aws_s3_bucket" "log_bucket" {
@@ -17,12 +17,12 @@ resource "aws_s3_bucket" "log_bucket" {
   }
 }
 
-module "firehose_stream" {
+module "formplayer_request_response_logs_firehose_stream" {
   source = "./firehose_stream"
   environment = "${var.environment}"
   account_id = "${var.account_id}"
   log_bucket_name = "${local.log_bucket_name}"
   log_bucket_arn = "${aws_s3_bucket.log_bucket.arn}"
-  log_bucket_prefix = "${local.log_bucket_prefix}"
-  log_bucket_error_prefix = "${local.log_bucket_error_prefix}"
+  log_bucket_prefix = "${local.formplayer_request_response_log_bucket_prefix}"
+  log_bucket_error_prefix = "${local.formplayer_request_response_log_bucket_error_prefix}"
 }
