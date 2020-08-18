0a041416a28038a7d6997fcf631728c7986c1bc0
Daniel Roberts <droberts@dimagi.com>
2020-08-17 20:13:18 -0500

    Fix test_secrets_list.py

--- src/commcare_cloud/environment/secrets/backends/ansible_vault/tests/test_secrets_list.py
+++ src/commcare_cloud/environment/secrets/backends/ansible_vault/tests/test_secrets_list.py
@@ -9,7 +9,7 @@ from commcare_cloud.environment.secrets.backends.ansible_vault.main import Ansib
 def test_generated_variables_as_expected():
     with open(os.path.join(os.path.dirname(__file__), 'expected-generated-variables.yml')) as f:
         expected_generated_variables = yaml.safe_load(f)
-    generated_variables = AnsibleVaultSecretsBackend().get_generated_variables()
+    generated_variables = AnsibleVaultSecretsBackend('a', 'b').get_generated_variables()
     assert generated_variables == expected_generated_variables, \
         yaml_diff(generated_variables, expected_generated_variables)
 
