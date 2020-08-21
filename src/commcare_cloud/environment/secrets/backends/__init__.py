914aaf14dee04a062348b03a97e24f23942d9967
Daniel Roberts <droberts@dimagi.com>
2020-08-21 16:05:18 -0500

    Add AwsSecretsBackend

--- src/commcare_cloud/environment/secrets/backends/__init__.py
+++ src/commcare_cloud/environment/secrets/backends/__init__.py
@@ -1,7 +1,9 @@
 from commcare_cloud.environment.secrets.backends.ansible_vault.main import AnsibleVaultSecretsBackend
+from commcare_cloud.environment.secrets.backends.aws_secrets.main import AwsSecretsBackend
 
 all_secrets_backends = [
     AnsibleVaultSecretsBackend,
+    AwsSecretsBackend,
 ]
 all_secrets_backends_by_name = {
     cls.name: cls
---
+++ src/commcare_cloud/environment/secrets/backends/aws_secrets/main.py
@@ -0,0 +1,22 @@
+from commcare_cloud.environment.secrets.backends.abstract_backend import AbstractSecretsBackend
+from commcare_cloud.environment.secrets.secrets_schema import get_generated_variables
+
+
+class AwsSecretsBackend(AbstractSecretsBackend):
+    name = 'aws-secrets'
+
+    def __init__(self, secret_name_prefix, environment):
+        self.secret_name_prefix = secret_name_prefix
+        self.environment = environment
+
+    @classmethod
+    def from_environment(cls, environment):
+        return AwsSecretsBackend(secret_name_prefix='commcare-{}'.format(environment.name), environment=environment)
+
+    def get_generated_variables(self):
+        return get_generated_variables(
+            lambda secret_spec: "lookup('aws_secret', '{}/{}')".format(self.secret_name_prefix, secret_spec.name))
+
+    def get_secret(self, var):
+        # Will implement this shortly
+        raise NotImplementedError
