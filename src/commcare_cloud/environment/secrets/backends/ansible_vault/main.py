3e5350d1112f84769c2aa7cb162d921b948efe88
Daniel Roberts <droberts@dimagi.com>
2020-08-21 15:14:32 -0500

    Pull out AnsibleVaultSecretsBackend.get_generated_variables into reusable function

--- src/commcare_cloud/environment/secrets/backends/ansible_vault/main.py
+++ src/commcare_cloud/environment/secrets/backends/ansible_vault/main.py
@@ -12,7 +12,7 @@ from six.moves import shlex_quote
 
 from commcare_cloud.environment.paths import ANSIBLE_DIR
 from commcare_cloud.environment.secrets.backends.abstract_backend import AbstractSecretsBackend
-from commcare_cloud.environment.secrets.secrets_schema import get_known_secret_specs
+from commcare_cloud.environment.secrets.secrets_schema import get_known_secret_specs, get_generated_variables
 
 
 class AnsibleVaultSecretsBackend(AbstractSecretsBackend):
@@ -40,25 +40,7 @@ class AnsibleVaultSecretsBackend(AbstractSecretsBackend):
 
     @staticmethod
     def get_generated_variables():
-        secret_specs = get_known_secret_specs()
-        secret_specs_by_name = {secret_spec.name: secret_spec for secret_spec in secret_specs}
-        generated_variables = {}
-        for secret_spec in secret_specs:
-            if secret_spec.ansible_var_lowercase:
-                ansible_var_name = secret_spec.name.lower()
-            else:
-                ansible_var_name = secret_spec.name
-            expression = secret_spec.get_legacy_reference()
-            for var_name in secret_spec.fall_back_to_vars:
-                expression += ' | default({})'.format(secret_specs_by_name[var_name].get_legacy_reference())
-            if not secret_spec.required:
-                if secret_spec.default_overrides_falsy_values:
-                    expression += ' | default({}, true)'.format(repr(secret_spec.default).strip('u'))
-                else:
-                    expression += ' | default({})'.format(repr(secret_spec.default).strip('u'))
-            if expression != secret_spec.name:  # avoid redundant `x: {{ x }}`
-                generated_variables[ansible_var_name] = "{{{{ {} }}}}".format(expression)
-        return generated_variables
+        return get_generated_variables()
 
     def _get_ansible_vault_password_and_record(self):
         """Get ansible vault password
--- src/commcare_cloud/environment/secrets/secrets_schema.py
+++ src/commcare_cloud/environment/secrets/secrets_schema.py
@@ -33,3 +33,25 @@ class SecretSpec(jsonobject.JsonObject):
 def get_known_secret_specs():
     with open(os.path.join(PACKAGE_BASE, 'environment', 'secrets', 'secrets.yml')) as f:
         return [SecretSpec.wrap(secret_spec) for secret_spec in yaml.safe_load(f)]
+
+
+def get_generated_variables():
+    secret_specs = get_known_secret_specs()
+    secret_specs_by_name = {secret_spec.name: secret_spec for secret_spec in secret_specs}
+    generated_variables = {}
+    for secret_spec in secret_specs:
+        if secret_spec.ansible_var_lowercase:
+            ansible_var_name = secret_spec.name.lower()
+        else:
+            ansible_var_name = secret_spec.name
+        expression = secret_spec.get_legacy_reference()
+        for var_name in secret_spec.fall_back_to_vars:
+            expression += ' | default({})'.format(secret_specs_by_name[var_name].get_legacy_reference())
+        if not secret_spec.required:
+            if secret_spec.default_overrides_falsy_values:
+                expression += ' | default({}, true)'.format(repr(secret_spec.default).strip('u'))
+            else:
+                expression += ' | default({})'.format(repr(secret_spec.default).strip('u'))
+        if expression != secret_spec.name:  # avoid redundant `x: {{ x }}`
+            generated_variables[ansible_var_name] = "{{{{ {} }}}}".format(expression)
+    return generated_variables
