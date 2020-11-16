c89276a92adf4068cb9f13860338340aec2c9c0a
Graham Herceg <g.a.herceg@gmail.com>
2020-11-16 13:49:38 -0500

    update open calls in main.py

--- src/commcare_cloud/python_migration_utils.py
+++ src/commcare_cloud/python_migration_utils.py
@@ -14,3 +14,14 @@ def open_for_json_dump(path):
     if six.PY2:
         return open(path, "wb")
     return open(path, "w", encoding="utf-8")
+
+
+def open_for_yaml_dump(path):
+    """
+    This ensures compatible file write modes based on Python versions
+    Python 2 json.dump and json.dumps create a bytes object
+    Python 3 json.dump and json.dumps create a str object
+    """
+    if six.PY2:
+        return open(path, "wb")
+    return open(path, "w", encoding="utf-8")
