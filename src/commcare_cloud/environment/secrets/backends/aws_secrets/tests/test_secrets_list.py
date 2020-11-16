9a5642d1d842a5977fa23fc066ab1f07abd3cf17
Graham Herceg <g.a.herceg@gmail.com>
2020-11-16 13:59:35 -0500

    update secrets tests open calls

--- src/commcare_cloud/environment/secrets/backends/aws_secrets/tests/test_secrets_list.py
+++ src/commcare_cloud/environment/secrets/backends/aws_secrets/tests/test_secrets_list.py
@@ -1,15 +1,18 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
+
 import os
+from io import open
 
 import yaml
 
-from commcare_cloud.environment.secrets.backends.aws_secrets.main import AwsSecretsBackend
+from commcare_cloud.environment.secrets.backends.aws_secrets.main import (
+    AwsSecretsBackend,
+)
 from commcare_cloud.environment.secrets.utils import yaml_diff
 
 
 def test_generated_variables_as_expected():
-    with open(os.path.join(os.path.dirname(__file__), 'expected-generated-variables.yml')) as f:
+    with open(os.path.join(os.path.dirname(__file__), 'expected-generated-variables.yml'), encoding='utf-8') as f:
         expected_generated_variables = yaml.safe_load(f)
     generated_variables = AwsSecretsBackend('commcare-staging', environment=None).get_generated_variables()
     assert generated_variables == expected_generated_variables, \
