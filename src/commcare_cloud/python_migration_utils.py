d800746eea970af75bee749145f6e49504003198
Graham Herceg <g.a.herceg@gmail.com>
2020-11-16 14:33:49 -0500

    fix comment

--- src/commcare_cloud/python_migration_utils.py
+++ src/commcare_cloud/python_migration_utils.py
@@ -18,9 +18,7 @@ def open_for_json_dump(path):
 
 def open_for_yaml_dump(path):
     """
-    This ensures compatible file write modes based on Python versions
-    Python 2 json.dump and json.dumps create a bytes object
-    Python 3 json.dump and json.dumps create a str object
+    This ensures compatible file write modes based on Python versions for yaml.dump/safe_dump calls
     """
     if six.PY2:
         return open(path, "wb")
