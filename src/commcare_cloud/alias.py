1d274ce66c40521a41e0c043685b1f6e98924ada
Graham Herceg <g.a.herceg@gmail.com>
2020-11-24 11:19:27 -0500

    do not encode arguments

--- src/commcare_cloud/alias.py
+++ src/commcare_cloud/alias.py
@@ -24,9 +24,24 @@ def _encode_args(*args, **kwargs):
     return argv
 
 
+def _generate_args(*args, **kwargs):
+    argv = []
+
+    for arg in args:
+        argv.append(arg)
+    for key, value in kwargs.items():
+        if value is False or value is None:
+            continue
+        elif value is True:
+            argv.append('--{}'.format(key))
+        else:
+            argv.extend(['--{}'.format(key), value])
+    return argv
+
+
 def commcare_cloud(*args, **kwargs):
     from .cli_utils import print_command
     from .commcare_cloud import call_commcare_cloud
-    argv = _encode_args('commcare-cloud', *args, **kwargs)
+    argv = _generate_args('commcare-cloud', *args, **kwargs)
     print_command(argv)
     return call_commcare_cloud(argv)
