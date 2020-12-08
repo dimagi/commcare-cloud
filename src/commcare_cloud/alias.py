c5b5d434e9a0ef19a88d7f08d217bd24e7870487
Graham Herceg <g.a.herceg@gmail.com>
2020-12-08 10:25:14 -0500

    remove _encode_args

--- src/commcare_cloud/alias.py
+++ src/commcare_cloud/alias.py
@@ -1,27 +1,4 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
-import six
-
-
-def _encode_args(*args, **kwargs):
-    argv = []
-
-    def encode_string(value):
-        if isinstance(value, six.string_types + six.integer_types):
-            return six.text_type(value).encode('utf-8')
-        else:
-            TypeError("Do not know how to interpret type {} as a command-line argument: {}"
-                      .format(type(value), value))
-    for arg in args:
-        argv.append(six.text_type(arg).encode('utf-8'))
-    for key, value in kwargs.items():
-        if value is False or value is None:
-            continue
-        elif value is True:
-            argv.append('--{}'.format(key))
-        else:
-            argv.extend(['--{}'.format(key), encode_string(value)])
-    return argv
+from __future__ import absolute_import, unicode_literals
 
 
 def _generate_args(*args, **kwargs):
