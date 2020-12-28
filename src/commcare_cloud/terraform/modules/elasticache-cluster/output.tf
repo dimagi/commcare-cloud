e9e710cb191136dabea9fa841e1d7bcca9213ab0
ramesh.ganne <ramesh.ganne@fissionlabs.com>
2020-12-28 15:13:28 +0530

    updated terraform module-elasticache-cluster,r53-private-zone-create & env file.

--- src/commcare_cloud/terraform/modules/elasticache-cluster/output.tf
+++ src/commcare_cloud/terraform/modules/elasticache-cluster/output.tf
@@ -1,3 +1,3 @@
-output "configuration_endpoint_address" {
-  value = "${aws_elasticache_replication_group.redis-dev-cluster-0.configuration_endpoint_address}"
+output "primary_configuration_endpoint_address" {
+  value = "${aws_elasticache_replication_group.redis-dev-cluster-0.*.primary_endpoint_address}"
 }
