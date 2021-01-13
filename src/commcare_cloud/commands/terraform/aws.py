20149447618eac4f2c36e1e4e3ccc168d61f1085
Marco Perosa <mperosa@dimagi.com>
2021-01-13 15:18:37 -0500

    Fix datetime comparison

--- src/commcare_cloud/commands/terraform/aws.py
+++ src/commcare_cloud/commands/terraform/aws.py
@@ -8,6 +8,7 @@ import subprocess
 import textwrap
 from datetime import datetime
 from dateutil import parser
+import pytz
 from io import open
 
 import boto3
@@ -585,7 +586,7 @@ def _has_valid_session_credentials_for_sso():
 
         if 'startUrl' in contents and 'expiresAt' in contents:
             expiration = parser.isoparse(contents['expiresAt'])
-            return expiration
+            return pytz.utc.localize(datetime.utcnow()) < expiration
     return False
 
 
@@ -603,7 +604,7 @@ def _has_valid_v1_session_credentials(aws_profile):
     except configparser.NoOptionError:
         return False
 
-    return expiration
+    return pytz.utc.localize(datetime.utcnow()) < expiration
 
 
 def _iter_files_in_dir(directory):
