2be8d06f1e5f3ff847ca8cf6f4bfa57f19efcc74
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2021-02-26 14:07:42 +0530

    update existing firehose configuration with hive partition format based prefix

--- src/commcare_cloud/terraform/modules/logshipping/main.tf
+++ src/commcare_cloud/terraform/modules/logshipping/main.tf
@@ -1,10 +1,7 @@
 locals {
   log_bucket_name = "dimagi-commcare-${var.environment}-logs"
-  partitioned_log_bucket_name = "dimagi-commcare-${var.environment}-partitioned-logs"
-  formplayer_request_response_log_bucket_prefix = "formplayer-request-response-logs-${var.environment}"
-  formplayer_request_response_log_bucket_error_prefix = "formplayer-request-response-logs-${var.environment}-error"
-  formplayer_request_response_log_bucket_prefix_partitioned = "formplayer-request-response-logs-partitioned-${var.environment}"
-  formplayer_request_response_log_bucket_error_prefix_partitioned = "formplayer-request-response-logs-partitioned-${var.environment}-error"
+  formplayer_request_response_log_bucket_prefix = "formplayer-request-response-logs-partitioned-${var.environment}"
+  formplayer_request_response_log_bucket_error_prefix = "formplayer-request-response-logs-partitioned-${var.environment}-error"
   hive_prefix = "year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}"
   hive_error_prefix = "!{firehose:random:string}/!{firehose:error-output-type}/!{timestamp:yyyy/MM/dd}"
 }
@@ -22,37 +19,14 @@ resource "aws_s3_bucket" "log_bucket" {
   }
 }
 
-resource "aws_s3_bucket" "partitioned_log_bucket" {
-  bucket = "${local.partitioned_log_bucket_name}"
-  acl = "private"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
 module "formplayer_request_response_logs_firehose_stream" {
   source = "./firehose_stream"
   environment = "${var.environment}"
   account_id = "${var.account_id}"
   log_bucket_name = "${local.log_bucket_name}"
   log_bucket_arn = "${aws_s3_bucket.log_bucket.arn}"
-  log_bucket_prefix = "${local.formplayer_request_response_log_bucket_prefix}"
-  log_bucket_error_prefix = "${local.formplayer_request_response_log_bucket_error_prefix}"
+  log_bucket_prefix = "${local.formplayer_request_response_log_bucket_prefix}/${local.hive_prefix}"
+  log_bucket_error_prefix = "${local.formplayer_request_response_log_bucket_error_prefix}/${local.hive_error_prefix}"
   firehose_stream_name = "${local.formplayer_request_response_log_bucket_prefix}"
 }
 
-module "formplayer_request_response_logs_firehose_stream_partitioned" {
-  source = "./firehose_stream"
-  environment = "${var.environment}"
-  account_id = "${var.account_id}"
-  log_bucket_name = "${local.partitioned_log_bucket_name}"
-  log_bucket_arn = "${aws_s3_bucket.partitioned_log_bucket.arn}"
-  log_bucket_prefix = "${local.formplayer_request_response_log_bucket_prefix_partitioned}/${local.hive_prefix}"
-  log_bucket_error_prefix = "${local.formplayer_request_response_log_bucket_error_prefix_partitioned}/${local.hive_error_prefix}"
-  firehose_stream_name = "${local.formplayer_request_response_log_bucket_prefix_partitioned}"
-}
