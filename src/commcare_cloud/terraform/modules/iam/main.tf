b3b1ade5e561886271d2ce4ce3ce2073c0b52240
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2021-05-13 19:51:06 +0530

    adding IAM role to use with control1-machine and copy formplayer logs into new folder

--- src/commcare_cloud/terraform/modules/iam/main.tf
+++ src/commcare_cloud/terraform/modules/iam/main.tf
@@ -24,3 +24,32 @@ resource "aws_iam_policy" "force_mfa" {
   description = "This policy allows users to manage their own passwords and MFA devices but nothing else unless they authenticate with MFA"
   policy = "${file("${path.module}/EnforceMFAPolicy.json")}"
 }
+
+resource "aws_iam_role" "formplayerlogbucket_role" {
+  name = "formplayerlogbucket_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+          "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "formplayerlog_policy" {
+  name = "formplayerlog_bucket_policy"
+  policy = "${file("${path.module}/FormplayerS3Policy.json")}"
+  }
+
+resource "aws_iam_role_policy_attachment" "formplayerlogbucket_roleattachment" {
+  role       = "${aws_iam_role.formplayerlogbucket_role.name}"
+  policy_arn = "${aws_iam_policy.formplayerlog_policy.arn}"
+}
