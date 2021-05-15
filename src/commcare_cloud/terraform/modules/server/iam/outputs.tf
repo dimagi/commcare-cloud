bc1d4c6fcad5ede57af23dc2daa3ab0ecc0ecef5
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2021-05-15 18:21:28 +0530

    minor modifications to the formplayerlogbucket_IAM role, retriving profile in outputs and sample code to modify role for temporary actions

--- src/commcare_cloud/terraform/modules/server/iam/outputs.tf
+++ src/commcare_cloud/terraform/modules/server/iam/outputs.tf
@@ -6,3 +6,10 @@ output "commcare_server_instance_profile" {
   value = "${aws_iam_instance_profile.commcare_server_instance_profile.name}"
 }
 
+output "formplayer_log_role_arn" {
+  value = "${aws_iam_role.formplayer_log_role.arn}"
+}
+
+output "formplayerlogbucket_instance_profile" {
+  value = "${aws_iam_instance_profile.formplayerlogbucket_instance_profile.name}"
+}
