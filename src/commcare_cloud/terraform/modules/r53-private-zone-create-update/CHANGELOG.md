b2a7a4afcde16333fc131719f5d9ca29efa3f043
ramesh.ganne <ramesh.ganne@fissionlabs.com>
2021-05-20 10:00:13 +0530

    merged and resolved the conflict in modules/server/iam/main.tf

--- src/commcare_cloud/terraform/modules/r53-private-zone-create-update/CHANGELOG.md
+++ src/commcare_cloud/terraform/modules/r53-private-zone-create-update/CHANGELOG.md
@@ -1,2 +1,4 @@
+# r53-private-zone-create-update-v0.2.0
+[rameshganne] - Updating module to support Terraform v12 or higher.
 # r53-private-zone-create-update-v0.1.0
 [rameshganne] - Initial commit create hosted-zone & associate to existing VPC( means adding to private zone ).
--- src/commcare_cloud/terraform/modules/r53-private-zone-create-update/main.tf
+++ src/commcare_cloud/terraform/modules/r53-private-zone-create-update/main.tf
@@ -1,17 +1,17 @@
 resource "aws_route53_zone" "create-private-r53-zone" {
-  count  = "${var.create == "true" ? 1 : 0}"
-  name   = "${var.domain_name}"
+  count  = var.create == true ? 1 : 0
+  name   = var.domain_name
 
   vpc {
-    vpc_id = "${var.zone_vpc_id}"
+    vpc_id = var.zone_vpc_id
   }
 }
 
 resource "aws_route53_record" "create-record-r53-pri-zone" {
-  count   = "${var.create_record == "true" ? 1 : 0}"
-  zone_id = "${aws_route53_zone.create-private-r53-zone.zone_id}"
-  name    = "${var.route_names}"
-  type    = "${var.type}"
-  ttl     = "${var.ttl}"
-  records = ["${var.records}"]
+  count   = var.create_record == true ? 1 : 0
+  zone_id = aws_route53_zone.create-private-r53-zone[count.index].zone_id
+  name    = var.route_names
+  type    = var.type
+  ttl     = var.ttl
+  records = var.records
 }
