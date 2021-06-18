1c91b6f84017b575823ab76a8bf809d8ed4c0b88
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2021-06-18 07:04:19 +0530

    preparing plan to delete control1-production server and formplayer_logbucket_role

--- src/commcare_cloud/terraform/modules/server/iam/outputs.tf
+++ src/commcare_cloud/terraform/modules/server/iam/outputs.tf
@@ -5,11 +5,3 @@ output "commcare_server_role_arn" {
 output "commcare_server_instance_profile" {
   value = "${aws_iam_instance_profile.commcare_server_instance_profile.name}"
 }
-
-output "formplayer_log_role_arn" {
-  value = "${aws_iam_role.formplayer_log_role.arn}"
-}
-
-output "formplayerlogbucket_instance_profile" {
-  value = "${aws_iam_instance_profile.formplayerlogbucket_instance_profile.name}"
-}
