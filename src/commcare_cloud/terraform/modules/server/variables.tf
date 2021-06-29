fc986ced87ba080fa7b75179a664b2f564bc2501
ramesh.ganne <ramesh.ganne@fissionlabs.com>
2021-06-29 12:34:10 +0530

    creating EC2 status check alarms-server module

--- src/commcare_cloud/terraform/modules/server/variables.tf
+++ src/commcare_cloud/terraform/modules/server/variables.tf
@@ -36,3 +36,30 @@ variable "metadata_tokens" {
   description = "Whether or not the metadata service requires session tokens, also referred to as Instance Metadata Service Version 2 (IMDSv2). Valid values include optional or required. Defaults to optional"
   default     = "optional"
 }
+variable "server_auto_recovery" {
+  type        = bool
+}
+
+variable "name_format_system" {
+  description = "Naming scheme as a string, to use with the format() function."
+  default     = "%s-system-auto-recover-%02d"
+  type        = string
+}
+
+variable "name_format_instance" {
+  description = "Naming scheme as a string, to use with the format() function."
+  default     = "%s-instance-auto-recover-%02d"
+  type        = string
+}
+
+variable "maximum_failure_duration" {
+  description = "Maximum amount of system status checks failures period in seconds before recovery kicks in."
+  default     = "60"
+  type        = string
+}
+
+variable "alarm_actions" {
+  description = "list of alarm actions to append to the default (optional)"
+  default     = []
+  type        = list(string)
+}
