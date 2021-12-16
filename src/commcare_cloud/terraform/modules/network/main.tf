2148c12df205fb70898db8716d1542fd6d51371a
pvisweswar <visweswar.pathri@fissionlabs.com>
2021-12-16 11:38:41 +0530

    Restricting_s3_access_by_VPC_endpoint

--- src/commcare_cloud/terraform/modules/network/main.tf
+++ src/commcare_cloud/terraform/modules/network/main.tf
@@ -559,3 +559,12 @@ resource "aws_security_group" "elasticache" {
     Name = "elasticache-${var.env}"
   }
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-east-1.s3"
+}
+resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = aws_route_table.private.id
+}
