123080b031ea1f6c5c8abb438abf02d7802dc73a
Sravan Reddy <sravfeyn@gmail.com>
2022-01-18 16:28:30 +0530

    Add summary and do cchq deploy-stack

--- oneclick_monolith_install/cchq-install.sh
+++ oneclick_monolith_install/cchq-install.sh
@@ -45,7 +45,7 @@ touch /var/log/ansible.log && chmod 666 /var/log/ansible.log
 
 
 printf "\nStep 3: Initializing environments directory for storing your CommCareHQ instance's configuration \n"
-ansible-playbook --connection=local --extra-vars "@$config_file_path" "$DIR/bootstrap-env-playbook.yml" 
+ansible-playbook --connection=local --extra-vars "@$config_file_path" "$DIR/bootstrap-env-playbook.yml"
 printf "\n Encrypting your environment's passwords file using ansible-vault. Please store this password safely as it will be required latter \n"
 ansible-vault encrypt ~/environments/$env_name/vault.yml
 
@@ -53,5 +53,21 @@ printf "\bStep 4: Setting up users and SSH auth \n"
 source ~/.commcare-cloud/load_config.sh
 commcare-cloud $env_name update-local-known-hosts
 commcare-cloud $env_name bootstrap-users -c local
-printf "\ncommcare-cloud setup is successful! You can now install CommCareHQ using below command\n"
-printf "commcare-cloud $env_name deploy-stack --skip-check --skip-tags=users -e 'CCHQ_IS_FRESH_INSTALL=1' -c local \n"
+
+printf "\nEverything is setup to install CommCareHQ now!\n"
+printf "Please see below a summary of what this script has setup so far!\n"
+printf "1. Installed commcare-cloud, the tool to deploy and manage your CommCareHQ instance.\n"
+printf "2. Users ansible and $ssh_username are created with SSH and sudo access.\n"
+printf "3. A configuration directory created for your CommCareHQ environment at: $HOME/environments/env_name.\n"
+printf "  This directory has all the configuration and the encrypted vault file containing passwords.\n"
+printf "  The vault file also has the sudo password for the ansible user under the key ansible_sudo_pass\n\n"
+
+printf "You can now install CommCareHQ using below command\n\n"
+printf "commcare-cloud $env_name deploy-stack --skip-check --skip-tags=users -e 'CCHQ_IS_FRESH_INSTALL=1' -c local \n\n"
+printf "Would you like to run the above command now?\n"
+read -p "(Please note that if this command fails midway, you can run this command directly instead of rerunning the cchq-install command) Proceed (Y/n)?\n" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    commcare-cloud $env_name deploy-stack --skip-check --skip-tags=users -e 'CCHQ_IS_FRESH_INSTALL=1' -c local
+fi
