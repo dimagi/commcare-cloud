75efe4a40c151559a5370709c79d5029ee03c1b0
Sravan Reddy <sravfeyn@gmail.com>
2022-01-19 14:57:07 +0530

    remove unused keys

--- quick_monolith_install/cchq-install.sh
+++ quick_monolith_install/cchq-install.sh
@@ -46,7 +46,7 @@ touch /var/log/ansible.log && chmod 666 /var/log/ansible.log
 
 printf "\nStep 3: Initializing environments directory for storing your CommCareHQ instance's configuration \n"
 ansible-playbook --connection=local --extra-vars "@$config_file_path" "$DIR/bootstrap-env-playbook.yml"
-printf "\n Encrypting your environment's passwords file using ansible-vault. Please store this password safely as it will be required latter \n"
+printf "\n Encrypting your environment's passwords file using ansible-vault. Please store this password safely as it will be required later \n"
 ansible-vault encrypt ~/environments/$env_name/vault.yml
 
 printf "\bStep 4: Setting up users and SSH auth \n"
@@ -58,13 +58,13 @@ printf "\nEverything is setup to install CommCareHQ now!\n"
 printf "Please see below a summary of what this script has setup so far!\n"
 printf "1. Installed commcare-cloud, the tool to deploy and manage your CommCareHQ instance.\n"
 printf "2. Users ansible and $ssh_username are created with SSH and sudo access.\n"
-printf "3. A configuration directory created for your CommCareHQ environment at: $HOME/environments/env_name.\n"
+printf "3. A configuration directory created for your CommCareHQ environment at: $HOME/environments/$env_name.\n"
 printf "  This directory has all the configuration and the encrypted vault file containing passwords.\n"
 printf "  The vault file also has the sudo password for the ansible user under the key ansible_sudo_pass\n\n"
 
 printf "You can now install CommCareHQ using below command\n\n"
 printf "commcare-cloud $env_name deploy-stack --skip-check --skip-tags=users -e 'CCHQ_IS_FRESH_INSTALL=1' -c local \n\n"
-printf "Would you like to run the above command now?\n"
+printf "Would you like the above command to be run now?\n"
 read -p "(Please note that if this command fails midway, you can run this command directly instead of rerunning the cchq-install command) Proceed (Y/n)?\n" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
