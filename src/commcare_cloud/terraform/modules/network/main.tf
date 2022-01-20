c9332b0301a3147955105fce0dd12d30b968f5d0
pvisweswar <visweswar.pathri@fissionlabs.com>
2022-01-20 18:05:40 +0530

    region_name

--- src/commcare_cloud/terraform/modules/network/main.tf
+++ src/commcare_cloud/terraform/modules/network/main.tf
@@ -560,7 +560,6 @@ resource "aws_security_group" "elasticache" {
   }
 }
 
-region = "$(var.region_name)"
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.${var.region}.s3"
--- src/commcare_cloud/terraform/modules/network/variables.tf
+++ src/commcare_cloud/terraform/modules/network/variables.tf
@@ -28,5 +28,5 @@ variable "vpn_connection_routes" {
 }
 
 variable "region" {
-  description = "This is region value"
+ type = string 
 }
