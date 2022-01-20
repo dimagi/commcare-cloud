a31b00c8f3a6363c0c7928b395e4dd1e75a6cc59
pvisweswar <visweswar.pathri@fissionlabs.com>
2022-01-20 19:12:03 +0530

    region_name

--- src/commcare_cloud/terraform/modules/network/main.tf
+++ src/commcare_cloud/terraform/modules/network/main.tf
@@ -560,7 +560,6 @@ resource "aws_security_group" "elasticache" {
   }
 }
 
-region_name = "${var.region}"
 resource "aws_vpc_endpoint" "s3" {
   vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.${var.region}.s3"
