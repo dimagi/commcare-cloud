c2738c501d96d4399a74d36eebc65824b8bf9808
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2022-04-07 18:50:57 +0530

    forcing to use IMDS-v2

--- src/commcare_cloud/terraform/modules/server/variables.tf
+++ src/commcare_cloud/terraform/modules/server/variables.tf
@@ -34,7 +34,7 @@ variable "az" {}
 variable "group_tag" {}
 variable "metadata_tokens" {
   description = "Whether or not the metadata service requires session tokens, also referred to as Instance Metadata Service Version 2 (IMDSv2). Valid values include optional or required. Defaults to optional"
-  default     = "optional"
+  default     = "required"
 }
 variable "server_auto_recovery" {
   type        = bool
