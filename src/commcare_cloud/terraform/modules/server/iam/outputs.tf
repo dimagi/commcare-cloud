c9c167ef4f4f7aa4cb11c351e68e5e33f2172b3f
pvisweswar <visweswar.pathri@fissionlabs.com>
2022-06-06 13:54:38 +0530

    tf_14upgrade_warnings_cleanup

--- src/commcare_cloud/terraform/modules/server/iam/outputs.tf
+++ src/commcare_cloud/terraform/modules/server/iam/outputs.tf
@@ -1,7 +1,7 @@
 output "commcare_server_role_arn" {
-  value = "${aws_iam_role.commcare_server_role.arn}"
+  value = aws_iam_role.commcare_server_role.arn
 }
 
 output "commcare_server_instance_profile" {
-  value = "${aws_iam_instance_profile.commcare_server_instance_profile.name}"
+  value = aws_iam_instance_profile.commcare_server_instance_profile.name
 }
