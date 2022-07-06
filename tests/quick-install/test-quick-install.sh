6435bdc31f12b5861639290b1beb7837e8d1f05e
Sravan Reddy <sravfeyn@gmail.com>
2022-07-06 13:48:59 +0530

    Fix paths

--- tests/quick-install/test-quick-install.sh
+++ tests/quick-install/test-quick-install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -e
 
-cd commcare-cloud/quick_monolith_install
-bash cchq-install.sh ../tests/test-install-config.yml
+cd ./quick_monolith_install
+bash cchq-install.sh ../tests/quick-install/test-install-config.yml
