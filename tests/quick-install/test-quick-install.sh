fe82b544ba67c9941582e2aed356f61458a8e429
Sravan Reddy <sravfeyn@gmail.com>
2022-07-06 14:34:44 +0530

    Link github

--- tests/quick-install/test-quick-install.sh
+++ tests/quick-install/test-quick-install.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 set -e
 
+GROUP_VARS=src/commcare_cloud/ansible/group_vars/all.yml
+eval PYVER=$(grep python_version: $GROUP_VARS | head -n1 | cut -d' ' -f2)
+# Link GitHub Actions-installed python where ansible virtualenv will find it
+[ -z $(which python${PYVER}) ] && sudo ln -s /opt/python/${PYVER}/bin/*${PYVER}* /usr/local/bin/
+which python${PYVER}
+python${PYVER} --version
+
 cd ./quick_monolith_install
 bash cchq-install.sh ../tests/quick-install/test-install-config.yml
