918795d418a3190ad88824ba8186259c2aaa288c
Charl Smit <csmit@dimagi.com>
2022-08-10 15:34:38 +0200

    Wrap files up a bit

---
+++ quick_cluster_install/README.md
@@ -0,0 +1,62 @@
+## Automated cluster setup
+
+### Prerequisites
+Make sure the aws cli tool is installed on your local machine
+
+1) Add the following to `~/.aws/config`:
+
+```
+[profile commcare-cluster-test:session]
+region = us-west-2
+output = json
+
+Add the following to ~/.aws/credentials:
+[commcare-cluster-test]
+aws_access_key_id = <access_key_id_from_keypass>
+aws_secret_access_key = <secret_access_key_from_keypass>
+```
+
+2) Copy the commcarehq_cluster_testing.pem file to your local ssh directory. 
+
+
+### Set up the cluster
+
+Setting up the cluster basically involves
+1) Configuring the cluster environment file
+2) Provision machines
+3) Deploy commcarehq from control machine
+
+> **_NOTE:_** 
+> Branch from `automated-cluster-setup` and checkout to that branch before you begin
+
+For the purposes of this process, let's suppose you want to create a new cluster environment called `cluster`.
+
+#### Step 1: Configuring the cluster environment file
+Change directory 
+> cd quick_cluster_install/
+
+Run the following command:
+> cp sample_environment.yml.sample cluster.yml 
+
+Fill out the new environment's details.
+
+#### Step 2: Provision machines
+
+After you've filled out the details in this new `cluster.yml` file, you simply run the following command to provision
+the cluster machines on AWS:
+> ./provision_machines.sh cluster
+
+The script will do the following:
+1) Check if the `cluster` environment exists; if not, it will simply copy the `sample_environment` environment. 
+   The new environment's structure will look as follows:
+    - _install_config.yml_: this file contains basic configurations, like the username of the user to set up on the server, etc.
+    - _known_hosts_: this file is automatically created to be used during deploy.
+    - _spec.yml_: this file contains the details of what the cluster looks like, i.e. what services sits on what machines, etc., and is used to provision the machines and populate the rest of the environment files necessary for a deploy.
+
+> **_NOTE:_** 
+> If you wish to specify your environment’s own spec.yml and not use the default spec.yml, you can either alter the default spec in the `sample_environment` 
+> environment or create your own environment manually and specify it there. if you decide to create your own environment manually, please take notice of the file structure mentioned above.
+
+#### Step 3: Deploy commcarehq from control machine
+When the script is done, follow the output command to ssh into the remote control machine. If you want to do this later you can find the control machine’s IP in the newly created environment’s inventory.ini file (remember, you will need to specify the `.pem` identity file).
+
---
+++ quick_cluster_install/provision_machines.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+set -e
+
+COMMCARE_CLOUD_ROOT=$(dirname $(dirname $(readlink -f $0)))
+CLUSTER_ENVIRONMENTS_DIR=$COMMCARE_CLOUD_ROOT/quick_cluster_install/environments
+
+ENV=$1
+ENVIRONMENT_DIR=$CLUSTER_ENVIRONMENTS_DIR/$ENV
+
+SPEC=$ENVIRONMENT_DIR/spec.yml
+RESERVED_GIT_BRANCH="automated-cluster-setup"
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+SAMPLE_ENV="sample_environment"
+
+# Set commcare-cloud's environments directory
+export COMMCARE_CLOUD_ENVIRONMENTS=$CLUSTER_ENVIRONMENTS_DIR
+
+check_environment() {
+
+#  if [ "${BRANCH}" == "${RESERVED_GIT_BRANCH}" ] ; then
+#    echo "Please use another branch"
+#    exit 1
+#  fi
+
+  if [ "${ENV}" == "${SAMPLE_ENV}" ] ; then
+    echo "Please specify a different environment"
+    exit 1
+  fi
+
+  if [ ! -d $ENVIRONMENT_DIR ] ; then
+    echo "Should I create environment '${ENV}' at ${ENVIRONMENT_DIR}? [y/n]"
+    read create_env
+
+    if [ "${create_env}" != "y" ] ; then
+      echo "No action"
+      exit 0
+    fi
+
+    echo ""
+    echo "Creating environment '${ENV}' at ${ENVIRONMENT_DIR}"
+    echo ""
+
+    cp -r $COMMCARE_CLOUD_ENVIRONMENTS/$DEFAULT_ENV $ENVIRONMENT_DIR
+  fi
+
+  if [ ! -f $SPEC ] ; then
+    echo "No specifications file found!"
+    exit 1
+  fi
+
+  KNOWN_HOSTS_FILE=$ENVIRONMENT_DIR/known_hosts
+
+  if [ ! -f $KNOWN_HOSTS_FILE ] ; then
+    touch $KNOWN_HOSTS_FILE
+  fi
+}
+
+copy_install_config() {
+  INSTALL_CONFIG_FILE_ORIGIN=$COMMCARE_CLOUD_ROOT/quick_cluster_install/$ENV.yml
+  INSTALL_CONFIG_FILE_DESTINATION=$ENVIRONMENT_DIR/install-config.yml
+
+  if [ ! -f $INSTALL_CONFIG_FILE_ORIGIN ] ; then
+    echo "${INSTALL_CONFIG_FILE_ORIGIN} not found"
+    exit 1
+  fi
+
+  cp $INSTALL_CONFIG_FILE_ORIGIN $INSTALL_CONFIG_FILE_DESTINATION
+  echo "env_name: ${ENV}" >> $INSTALL_CONFIG_FILE_DESTINATION
+}
+
+encrypt_vault() {
+  ansible-vault encrypt $ENVIRONMENT_DIR/vault.yml
+}
+
+sync_to_git() {
+  # Check if there's new changes
+  if [[ `git status --porcelain` ]]; then
+      git add .
+      git commit -m "Update ${ENV} files"
+      git push origin $BRANCH
+
+      if [ $? -ne 0 ] ; then
+        echo "Push to git failed"
+        exit 1
+      fi
+  fi
+}
+
+# Verify that environment exits, otherwise prompt to create it
+check_environment $1
+copy_install_config $1
+
+## Provision and create environment files
+python $COMMCARE_CLOUD_ROOT/commcare-cloud-bootstrap/commcare_cloud_bootstrap.py provision $SPEC --env $ENV
+
+CONTROL_HOST="${ENV}-control-0"
+CONTROL_IP=
+
+encrypt_vault $1
+sync_to_git $1
+
+INVENTORY_FILE=$ENVIRONMENT_DIR/inventory.ini
+AWS_PEM_FILE=$(grep "pem" "${SPEC}" | grep -o -P '(?<=pem: ).*')
+
+ansible-playbook $COMMCARE_CLOUD_ROOT/quick_cluster_install/bootstrap_basic.yml -i $INVENTORY_FILE --extra-vars "control_host=${CONTROL_HOST} git_branch=${BRANCH} pem_file_path=${AWS_PEM_FILE}" --private-key $AWS_PEM_FILE
+
+echo ""
+echo ""
+echo "All done! Now SSH into the control machine"
+echo ""
+echo "ssh -i ${AWS_PEM_FILE} ubuntu@${CONTROL_IP}"
