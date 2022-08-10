4876260a99c3aa06ecae300eb177401b96009b4b
prem-fissionhq <premkumar@Prem-MacBook-Pro.local>
2022-08-10 13:54:39 +0530

    checking whether we are in venv or not

--- src/commcare_cloud/commands/ansible/ansible_playbook.py
+++ src/commcare_cloud/commands/ansible/ansible_playbook.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import os
 import re
 import subprocess
+import ansible
 from packaging import version
 from copy import deepcopy
 
@@ -109,14 +110,17 @@ def run_ansible_playbook(
     def ansible_playbook(environment, playbook, *cmd_args):
         # verifying cchq env ansible version before execution
         required_ansible_version = "4.2.0"
-        venv_ansible_version = subprocess.run(["ansible", "--version", "|", "head", "-n1", "|", "awk", "'{print $2}'"], stdout=subprocess.PIPE, universal_newlines=True)
-        ansible_version = str(venv_ansible_version.stdout).split()[1]
-
-        if (version.parse(ansible_version) < version.parse(str(required_ansible_version))):
-            puts(color_error(f"The version of ansible you have installed ({ansible_version}) is no longer supported."))
-            puts(color_notice(f"To upgrade from ansible {ansible_version} to {required_ansible_version} or above you will first have to uninstall the current version (due to an idiosyncratic issue)"))
-            puts(color_code("  pip uninstall ansible"))
-            puts(color_notice("before re-installing the supported version using your standard method."))
+        #venv_ansible_version = subprocess.run(["ansible", "--version", "|", "head", "-n1", "|", "awk", "'{print $2}'"], stdout=subprocess.PIPE, universal_newlines=True)
+        if os.getenv['VIRTUAL_ENV']:
+            ansible_version = ansible.__version__
+            if (version.parse(ansible_version) < version.parse(str(required_ansible_version))):
+                puts(color_error(f"The version of ansible you have installed ({ansible_version}) is no longer supported."))
+                puts(color_notice(f"To upgrade from ansible {ansible_version} to {required_ansible_version} or above you will first have to uninstall the current version (due to an idiosyncratic issue)"))
+                puts(color_code("  pip uninstall ansible"))
+                puts(color_notice("before re-installing the supported version using your standard method."))
+                return 2
+        else:
+            puts(color_notice("please activate the cchq virtualenv."))
             return 2
 
         if os.path.isabs(playbook):
