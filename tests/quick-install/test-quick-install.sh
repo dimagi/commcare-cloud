941567c50d951c73657c05bd6c4a7546ab0a1292
Sravan Reddy <sravfeyn@gmail.com>
2022-09-01 12:09:46 +0530

    declare in the context of test script

--- tests/quick-install/test-quick-install.sh
+++ tests/quick-install/test-quick-install.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 set -e
 
+echo 'abc123' >> ~/vault_pass.txt
+ANSIBLE_VAULT_PASSWORD_FILE=$(readlink -f ~/vault_pass.txt)
+
 cd ./quick_monolith_install
 bash -x cchq-install.sh ../tests/quick-install/test-install-config.yml
