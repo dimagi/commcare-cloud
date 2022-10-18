01264141b4609890f33dcee0d6be6ee5d0535ebc
Cal Ellowitz <calellowitz@gmail.com>
2022-10-18 15:32:18 +0200

    create s3 bucket for releases

---
+++ src/commcare_cloud/terraform/modules/autoscaling/variables.tf
@@ -0,0 +1,2 @@
+variable "release_bucket" {}
+variable "commcare_server_role_id" {}
--- src/commcare_cloud/terraform/modules/server/iam/outputs.tf
+++ src/commcare_cloud/terraform/modules/server/iam/outputs.tf
@@ -2,6 +2,10 @@ output "commcare_server_role_arn" {
   value = aws_iam_role.commcare_server_role.arn
 }
 
+output "commcare_server_role_id" {
+  value = aws_iam_role.commcare_server_role.id
+}
+
 output "commcare_server_instance_profile" {
   value = aws_iam_instance_profile.commcare_server_instance_profile.name
 }
