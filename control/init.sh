62492607ae3d924a7d4cfa772f692cf132772b1a
Graham Herceg <gherceg@dimagi.com>
2022-10-31 12:08:09 -0700

    delete 3.6 virtualenv

--- control/init.sh
+++ control/init.sh
@@ -22,7 +22,11 @@ if [ -z ${CI_TEST} ]; then
     if [[ $BIONIC_USE_SYSTEM_PYTHON == false ]] && hash python3.10 2>/dev/null && [[ $( source /etc/os-release; echo $VERSION_ID ) == 18.04 ]]; then
         # only append 3.10 if it is not already in the name
         if [[ $CCHQ_VIRTUALENV != *"3.10"* ]]; then
-          CCHQ_VIRTUALENV=$CCHQ_VIRTUALENV-3.10
+            # delete the 3.6 virtualenv if it exists
+            if [[ -d $VENV ]]; then
+                rm -rf $VENV
+            fi
+            CCHQ_VIRTUALENV=$CCHQ_VIRTUALENV-3.10
         fi
         VENV=~/.virtualenvs/$CCHQ_VIRTUALENV
     fi
