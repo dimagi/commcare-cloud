f403d6d1ddee568ce2e7179331680e594b2b05aa
Daniel Miller <dmiller@dimagi.com>
2022-11-01 07:50:25 -0400

    Add docstring to release_names filter

--- src/commcare_cloud/ansible/roles/formplayer/filter_plugins/filters.py
+++ src/commcare_cloud/ansible/roles/formplayer/filter_plugins/filters.py
@@ -7,6 +7,10 @@ class FilterModule:
         return {'release_names': self.release_names}
 
     def release_names(self, hostvars, play_hosts):
+        """Aggregate release names from play_hosts
+
+        Returns a list of releases that are common to all play hosts.
+        """
         def nameset(vars):
             return {basename(f["path"]) for f in vars["releases"]["files"]}
         if not (play_hosts and all("releases" in hostvars[h] for h in play_hosts)):
