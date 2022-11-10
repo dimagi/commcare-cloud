8bda5a69b2b46c8f28e7489d95f0bd0ad43d4dc7
Daniel Miller <dmiller@dimagi.com>
2022-11-10 08:49:01 -0500

    Comment about "deploy" role in fabric

--- src/commcare_cloud/fab/fabfile.py
+++ src/commcare_cloud/fab/fabfile.py
@@ -192,6 +192,7 @@ def env_common():
         'staticfiles': staticfiles,
         'staticfiles_primary': [staticfiles[0]],
         'lb': [],
+        # deploy contains one server to which commcare-hq is deployed
         # having deploy here makes it so that
         # we don't get prompted for a host or run deploy too many times
         'deploy': deploy,
