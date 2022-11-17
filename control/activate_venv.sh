4e95de33c747fb8b7e257d0089a08a480661a5c6
Graham Herceg <gherceg@dimagi.com>
2022-11-17 09:44:06 -0800

    use [[ ]] to use string pattern match

--- control/activate_venv.sh
+++ control/activate_venv.sh
@@ -25,7 +25,7 @@ fi
 # if on 18.04 with 3.10 installed, use cchq-3.10 unless $BIONIC_USE_SYSTEM_PYTHON is true
 if [ "$BIONIC_USE_SYSTEM_PYTHON" == "false" ] && hash python3.10 2>/dev/null && [ $( source /etc/os-release; echo "$VERSION_ID" ) == "18.04" ]; then
     # only append 3.10 if it is not already in the name
-    if [ "$CCHQ_VIRTUALENV" != *"3.10"* ]; then
+    if [[ "$CCHQ_VIRTUALENV" != *"3.10"* ]]; then
         # save for reference by callers
         CCHQ_VENV_PATH_OLD="$VENV"
         CCHQ_VIRTUALENV=$CCHQ_VIRTUALENV-3.10
