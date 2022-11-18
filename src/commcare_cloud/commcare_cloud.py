90f5c3966a901b61f88f10ec8b66b06ee34decda
Graham Herceg <gherceg@dimagi.com>
2022-11-18 11:40:50 -0800

    add space after source activate_venv command

--- src/commcare_cloud/commcare_cloud.py
+++ src/commcare_cloud/commcare_cloud.py
@@ -120,7 +120,7 @@ def run_on_control_instead(args, argv, force_latest_code):
         )
     else:
         bash_commands_template = (
-            '{env}source ~/commcare-cloud/control/activate_venv.sh --fail-on-error'
+            '{env}source ~/commcare-cloud/control/activate_venv.sh --fail-on-error '
             '&& {cchq} {cchq_args}'
         )
     cmd_parts = [
