2f2c161e644f56b1f5914d34ec2a696820252777
Chris Smit <chris.smit@dimagi.com>
2023-01-20 14:49:18 +0200

    Collect some basic os info

--- src/commcare_cloud/commands/ansible/ops_tool.py
+++ src/commcare_cloud/commands/ansible/ops_tool.py
@@ -8,7 +8,9 @@ import itertools
 import json
 import subprocess
 import sys
+import stat
 import os
+from pathlib import Path
 from collections import defaultdict
 from datetime import datetime
 from io import open
