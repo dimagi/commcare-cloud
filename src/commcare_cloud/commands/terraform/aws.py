e99cbe61650191521b96b977b7745dd37c6d6ba0
shyamkumarlchauhan <schauhan@dimagi.com>
2023-01-30 22:05:39 +0530

    internal nginx setup

--- src/commcare_cloud/commands/terraform/aws.py
+++ src/commcare_cloud/commands/terraform/aws.py
@@ -267,7 +267,7 @@ class AwsFillInventoryHelper(object):
         for proxy_internal_alb in self.environment.terraform_config.proxy_internal_albs:
             context.update(
                 self.get_host_group_definition(resource_name=proxy_internal_alb.name)
-
+            )
         return context
 
     @property
