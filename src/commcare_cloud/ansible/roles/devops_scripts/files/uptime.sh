421b6ae08b452dcf3c101f4702ea573808770cd1
pvisweswar <visweswar.pathri@fissionlabs.com>
2023-05-01 18:19:31 +0530

    updating_with_variable

--- src/commcare_cloud/ansible/roles/devops_scripts/files/uptime.sh
+++ src/commcare_cloud/ansible/roles/devops_scripts/files/uptime.sh
@@ -9,3 +9,17 @@ then
        x=logger '[Maintenance]  Rebooting server as uptime is greater than or equal to ninety days' && reboot`
        $x;
 fi
+
+#!/bin/bash
+
+#maintenance_time={{ maintenance_time }}
+#current_time=$(uptime | awk '{print $1}';)
+#uptime_days=`uptime | awk '/days?/ {print $3; next}; {print 0}'`
+#echo $uptime_days
+
+#if
+	#[[ $uptime_days -ge "90" ]] && [ $current_time = $maintenance_time ];
+#then
+       #x=logger '[Maintenance]  Rebooting server as uptime is greater than or equal to ninety days' && reboot`
+       #$x;
+#fi
