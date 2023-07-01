b60c067c0ef8c2194210ad23d423c52056ce3098
pvisweswar <visweswar.pathri@fissionlabs.com>
2023-07-01 16:14:11 +0530

    updating the files

--- src/commcare_cloud/ansible/roles/devops_scripts/files/uptime.sh
+++ src/commcare_cloud/ansible/roles/devops_scripts/files/uptime.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#uptime_days=`uptime | awk '/days?/ {print $3; next}; {print 0}'`
-#echo $uptime_days
-
-#if
-	#[[ $uptime_days -ge "90" ]] ;
-#then
-       #x=logger '[Maintenance]  Rebooting server as uptime is greater than or equal to ninety days' && reboot`
-       #$x;
-#fi
-
-#!/bin/bash
-
 maintenance_time={{ maintenance_time }}
 current_time=$(uptime | awk '{print $1}';)
 uptime_days=`uptime | awk '/days?/ {print $3; next}; {print 0}'`
