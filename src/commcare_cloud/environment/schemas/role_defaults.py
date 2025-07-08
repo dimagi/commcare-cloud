d5f38d2601a950020a0a722cc216273d634b53e0
Graham Herceg <gherceg@dimagi.com>
2025-07-08 10:53:38 -0700

    Remove unnecessary str wrapper
    Co-authored-by: Daniel Miller <dmiller@dimagi.com>

--- src/commcare_cloud/environment/schemas/role_defaults.py
+++ src/commcare_cloud/environment/schemas/role_defaults.py
@@ -16,7 +16,7 @@ def get_defaults_jsonobject(role, **kwargs):
     :return: The new JsonObject subclass.
     """
     cls = type(jsonobject.JsonObject)(
-        str("{}_Defaults".format(role)),
+        "{}_Defaults".format(role),
         (jsonobject.JsonObject,),
         dict(get_role_defaults(role), _allow_dynamic_properties=False, **kwargs),
     )
