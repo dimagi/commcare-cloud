b1c1ec45d21f0f9deda287d684482787473897cc
pvisweswar <visweswar.pathri@fissionlabs.com>
2025-10-07 16:51:23 +0530

    to solve lint error

--- src/commcare_cloud/environment/schemas/terraform.py
+++ src/commcare_cloud/environment/schemas/terraform.py
@@ -11,7 +11,12 @@ class TerraformConfig(jsonobject.JsonObject):
     username = jsonobject.StringProperty()
     password = jsonobject.StringProperty()
     terraform_version = jsonobject.StringProperty(
-        choices=['0.12', '0.13', '0.14', '0.15', '1.0', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '1.7', '1.8', '1.9'])
+        choices=[
+            '0.12', '0.13', '0.14', '0.15',
+            '1.0', '1.1', '1.2', '1.3',
+            '1.4', '1.5', '1.6', '1.7',
+            '1.8', '1.9'
+        ])
     manage_users = jsonobject.BooleanProperty(default=True)
     state_bucket = jsonobject.StringProperty()
     state_bucket_region = jsonobject.StringProperty()
