c327f172432ba938704bed0dff5bb9c4afa8ce5b
Daniel Miller <dmiller@dimagi.com>
2026-05-29 10:20:56 -0400

    Install ansible-galaxy roles and collections once
    The first command `ansible-galaxy install` installs both roles and
    collections, so the second `collection install` was redundant.
    
    Change -f to --force and comment about why it is used. The history of
    this option is intriguing:
    - 96ef9ec6df843f637e8d2521bceedb8cdfa7db06 - originally added, no reason given
    - 0584a4b51e4d2fd43e0a1251f121d33709958ab1 - removed, "way too slow"
    - 31b63b317e9fc89cd72c99f6f8c2c729b014d87e - re-added, no reason given

--- src/commcare_cloud/manage_commcare_cloud/install.py
+++ src/commcare_cloud/manage_commcare_cloud/install.py
@@ -30,17 +30,15 @@ class Install(CommandBase):
         env['ANSIBLE_ROLES_PATH'] = ANSIBLE_ROLES_PATH
         env['ANSIBLE_COLLECTIONS_PATHS'] = ANSIBLE_COLLECTIONS_PATHS
         requirements_yml = os.path.join(ANSIBLE_DIR, 'requirements.yml')
-        cmd_roles_parts = ['ansible-galaxy', 'install', '-f', '-r', requirements_yml]
-        cmd_collection_parts = ['ansible-galaxy', 'collection', 'install', '-f', '-r', requirements_yml]
-
-        for cmd_parts in (cmd_roles_parts, cmd_collection_parts):
-            cmd = ' '.join(shlex.quote(arg) for arg in cmd_parts)
-            print_command(cmd)
-            try:
-                subprocess.check_output(cmd, shell=True, env=env)
-            except subprocess.CalledProcessError as err:
-                print("process exited with error: %s" % err.returncode)
-                return err.returncode
+        # Uses --force to update roles when required versions change.
+        cmd_parts = ['ansible-galaxy', 'install', '--force', '-r', requirements_yml]
+        cmd = ' '.join(shlex.quote(arg) for arg in cmd_parts)
+        print_command(cmd)
+        try:
+            subprocess.check_output(cmd, shell=True, env=env)
+        except subprocess.CalledProcessError as err:
+            print("process exited with error: %s" % err.returncode)
+            return err.returncode
 
         puts(color_notice("To finish first-time installation, run `manage-commcare-cloud configure`"))
         return 0
