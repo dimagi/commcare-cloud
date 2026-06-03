f5a22f28feb1500b45f964159650027005a68558
Daniel Miller <dmiller@dimagi.com>
2026-06-03 11:03:52 -0400

    Add magic fixture fence
    Error on magic fixture use in commcare-cloud tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
-from unmagic import autouse
+from unmagic import autouse, fence
 
 from .fixtures import package_patches
 
 autouse(package_patches, __file__)
+fence.install(['commcare_cloud', 'tests'])
