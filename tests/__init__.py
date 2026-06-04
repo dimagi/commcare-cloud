df56a9b183824629784aa6527bf24e677d3c4598
Daniel Miller <dmiller@dimagi.com>
2026-06-04 16:32:57 -0400

    Fence unpackaged modules
    And remove unneeded __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ from unmagic import autouse, fence
 from .fixtures import package_patches
 
 autouse(package_patches, __file__)
-fence.install(['commcare_cloud', 'tests'])
+fence.install(['', 'commcare_cloud', 'tests'])
