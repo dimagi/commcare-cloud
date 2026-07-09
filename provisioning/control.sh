cda7454f831c80cc44f3ad9dc3d30932e764e07b
Daniel Miller <dmiller@dimagi.com>
2026-07-09 15:37:17 -0400

    Bind mount commcare-cloud repo
    - Reliable "real" file paths.
    - Use second bind mount to avoid error on symlinked git hook.
    - Set UV_PROJECT_ENVIRONMENT to avoid conflict with host venv.

--- provisioning/control.sh
+++ provisioning/control.sh
@@ -9,9 +9,24 @@ chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
 touch /var/log/ansible.log
 chown vagrant:vagrant /var/log/ansible.log
 
-ln -s /vagrant /home/vagrant/commcare-cloud
+# NOTE: if the UID of the host user is not 1000 (matching the vagrant
+# user's UID) then commcare-cloud files may not be writable.
+# Always edit files on the host rather than in the guest VM.
+CC_DIR=/home/vagrant/commcare-cloud
+sudo -u vagrant mkdir $CC_DIR
+mount --bind /vagrant $CC_DIR
+
+# fake pre-commit hook to avoid error in init.sh
+sudo -u vagrant mkdir -p /home/vagrant/.cc-hooks
+sudo -u vagrant cp $CC_DIR/git-hooks/pre-commit.sh /home/vagrant/.cc-hooks/pre-commit
+mount --bind /home/vagrant/.cc-hooks $CC_DIR/.git/hooks
+
+# custom venv because ~/commcare-cloud/.venv may be used by host
+echo 'export UV_PROJECT_ENVIRONMENT=~/.cc-venv' >> /home/vagrant/.profile
+
 snap install astral-uv --classic
-sudo -H -u vagrant bash -c 'export PATH=/snap/bin:$PATH; NO_INPUT=1 source /vagrant/control/init.sh'
+
+sudo -iu vagrant bash -c 'NO_INPUT=1 source ~/commcare-cloud/control/init.sh'
 
 echo "Provision completed! Now ssh into the control box by:
 
