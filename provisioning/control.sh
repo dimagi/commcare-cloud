6d5a062e437110e4990f647a870fe8d86ca40c0a
Daniel Miller <dmiller@dimagi.com>
2026-07-09 15:29:41 -0400

    Do not use sudo unnecessarily
    The script runs as root

--- provisioning/control.sh
+++ provisioning/control.sh
@@ -1,13 +1,13 @@
-sudo apt-get update
-sudo apt-get install -y libffi-dev libssl-dev git
+apt-get update
+apt-get install -y libffi-dev libssl-dev git
 
 ssh-keyscan 192.168.33.15 >> /home/vagrant/.ssh/known_hosts
 ssh-keyscan 192.168.33.16 >> /home/vagrant/.ssh/known_hosts
 ssh-keyscan 192.168.33.17 >> /home/vagrant/.ssh/known_hosts
-sudo chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
+chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
 
 ln -s /vagrant /home/vagrant/commcare-cloud
-sudo snap install astral-uv --classic
+snap install astral-uv --classic
 sudo -H -u vagrant bash -c 'export PATH=/snap/bin:$PATH; NO_INPUT=1 source /vagrant/control/init.sh'
 
 echo "Provision completed! Now ssh into the control box by:
