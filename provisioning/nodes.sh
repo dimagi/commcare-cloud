4eadff08028f37e36ccd324556c4a709f0f5e924
Daniel Miller <dmiller@dimagi.com>
2026-07-26 08:59:20 +0100

    Fix /opt/data permissions on Vagrant
    Disable ecryptfs environment because ES and Kafka fail to start:
    
    ecryptfs can't mmap shared libraries with exec permission, so JNA
    fails to load its native library out of /opt/data, which then makes
    ES's seccomp/system-call-filter bootstrap check fail and abort startup
    
    [WARN ][o.e.b.Natives] [es0] unable to load JNA native support library, native methods will be disabled.
    java.lang.UnsatisfiedLinkError: /opt/data/elasticsearch-6.8.23/tmp/jna8792826677444915978.tmp:
    /opt/data/elasticsearch-6.8.23/tmp/jna8792826677444915978.tmp: failed to map segment from shared object
    ...
    [INFO ][o.e.e.NodeEnvironment] [es0] using [1] data paths, mounts [[/opt/data (/opt/data)]], ..., types [ecryptfs]

--- provisioning/nodes.sh
+++ provisioning/nodes.sh
@@ -1,1 +1,5 @@
 sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+
+# Create data root so services are able to read the contents.
+# Otherwise it is created implicitly, and may not be readable by some services.
+mkdir --mode=755 /opt/data
